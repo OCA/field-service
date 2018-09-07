aac6717b7f834af7a1a2c4a67b4d0b7aa1c1eab8
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-09-07 16:44:47 -0500

    [ADD] fieldservice

---
+++ fieldservice/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service',
+    'summary': 'Locations, Orders, Calls',
+    'version': '11.0.0.0.1',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators, Odoo Communication Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/module_category.xml',
+        'security/res_groups.xml',
+        'views/menu.xml',
+    ],
+    'application': True,
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+        'max3903',
+    ],
+}
