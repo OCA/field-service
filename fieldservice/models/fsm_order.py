10291c64e1905b056d9ab6a601442de80b2f2da0
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-09-21 13:10:03 -0500

    [IMP] Various improvements

--- fieldservice/models/fsm_order.py
+++ fieldservice/models/fsm_order.py
@@ -8,10 +8,13 @@ class FSMOrder(models.Model):
     _name = 'fsm.order'
     _description = 'Field Service Order'
 
+    def _default_stage_id(self):
+        return self.env.ref('fieldservice.fsm_stage_new')
+
     name = fields.Char(string='Name', required=True)
     fsm_route_id = fields.Many2one('fsm.route', string='Route')
-    partner_id = fields.Many2one('res.partner', string='Customer',
-                                 domain=('customer', '=', True))
+    customer_id = fields.Many2one('res.partner', string='Customer',
+                                  domain=('customer', '=', True))
     fsm_location_id = fields.Many2one('fsm.location', string='Location')
     fsm_person_id = fields.Many2one('fsm.person',
                                     string='Field Service Person')
@@ -19,4 +22,7 @@ class FSMOrder(models.Model):
     scheduled_date = fields.Datetime(string='Scheduled Date')
     date = fields.Datetime(string='Date')
     description = fields.Char(string='Description')
-    fsm_stage_id = fields.Many2one('fsm.stage', string='Stage')
+    stage_id = fields.Many2one('fsm.stage', string='Stage',
+                               track_visibility='onchange',
+                               index=True,
+                               default=lambda self: self._default_stage_id())
