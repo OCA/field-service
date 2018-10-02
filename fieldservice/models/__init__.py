133c573f932c72cb21b536af5b2d757de05c54f6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-10-02 09:00:52 +0200

    [ADD] fieldservice 0.0.1 (#2)

---
+++ fieldservice/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    res_config_settings,
+    res_partner,
+    fsm_stage,
+    fsm_tag,
+    fsm_location,
+    fsm_person,
+    fsm_order,
+    fsm_route,
+)
