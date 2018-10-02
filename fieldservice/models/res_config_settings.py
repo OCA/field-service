5ca6138ae24c98c3b86cdbaf60f15c964f51384e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-10-02 09:00:52 +0200

    [ADD] fieldservice 0.0.1 (#2)

---
+++ fieldservice/models/res_config_settings.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+# from odoo import apit, _
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    group_fsm_team = fields.Boolean(
+        string='Manage Teams',
+        implied_group='fieldservice.group_fsm_team')
+    group_fsm_vehicle = fields.Boolean(
+        string='Manage Vehicles',
+        implied_group='fieldservice.group_fsm_vehicle')
