54597d3b36afd841bc0124379d59c6dc95578ebf
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-10-13 00:42:11 +0530

    [OCA]12567 Location structure needs to reflect hierarchy of Buildings (#22)
    * [IMP] Extend fsm.location object

--- fieldservice/models/fsm_location.py
+++ fieldservice/models/fsm_location.py
@@ -13,6 +13,24 @@ class FSMLocation(models.Model):
     partner_id = fields.Many2one('res.partner', string='Related Partner',
                                  required=True, ondelete='restrict',
                                  auto_join=True)
+    owner_id = fields.Many2one('res.partner', string='Related Owner',
+                               required=True, ondelete='restrict',
+                               auto_join=True)
+    customer_id = fields.Many2one('res.partner', string='Related Customer',
+                                  required=True, ondelete='restrict',
+                                  auto_join=True)
+    tag_ids = fields.Many2many('fsm.tag',
+                               string='Tags')
+    building = fields.Char(string='Building', size=35)
+    floor = fields.Char(string='Floor', size=35)
+    unit = fields.Char(string='Unit', size=35)
+    room = fields.Char(string='Room', size=35)
+    description = fields.Char(string='Description')
+    territory = fields.Char(string='Territory', size=35)
+    branch = fields.Char(string='Branch', size=35)
+    district = fields.Char(string='District', size=35)
+    region = fields.Char(string='Region', size=35)
+    timezone = fields.Char(string='Timezone', size=35)
 
     @api.model
     def create(self, vals):
