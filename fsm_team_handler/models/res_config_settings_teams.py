69acc920c3b3b38268ee2fbed3688e926a77a3fb
linto <linto@cybrosys.in>
2018-10-17 12:26:14 +0530

    Added access rules for skills, removed reference of red from the code

--- fsm_team_handler/models/res_config_settings_teams.py
+++ fsm_team_handler/models/res_config_settings_teams.py
@@ -6,6 +6,6 @@ from odoo import fields, models
 class ResConfigSettingsFsmTeam(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    group_red_fsm_team = fields.Boolean(
-        string='Manage RedFSM Teams',
-        implied_group='fsm_team_handler.group_red_fsm_team')
+    group_fsm_teams = fields.Boolean(
+        string='Manage FSM Teams',
+        implied_group='fsm_team_handler.group_fsm_teams')
--- fsm_team_handler/models/skills.py
+++ fsm_team_handler/models/skills.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 class SkillsHandler(models.Model):
     _name = 'fsm.skills'
     _rec_name = 'name'
-    _description = 'Manages skills'
+    _description = 'Manage skills'
 
     name = fields.Char(
         string="Skill Name",
