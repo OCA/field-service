b0a48f31d5e038a6d381e437842b4e0d2f6dc745
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-10-18 17:43:03 -0500

    [ADD] location, territory, branch, district, region

--- fieldservice/models/fsm_location.py
+++ fieldservice/models/fsm_location.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import pytz
+
 from odoo import api, fields, models
 
 
@@ -8,6 +10,13 @@ class FSMLocation(models.Model):
     _name = 'fsm.location'
     _inherits = {'res.partner': 'partner_id'}
     _description = 'Field Service Location'
+
+    @api.model
+    def _tz_get(self):
+        return [(tz, tz) for tz in sorted(pytz.all_timezones,
+                                          key=lambda tz: tz
+                                          if not tz.startswith('Etc/')
+                                          else '_')]
 
     direction = fields.Char(string='Directions')
     partner_id = fields.Many2one('res.partner', string='Related Partner',
@@ -21,18 +30,27 @@ class FSMLocation(models.Model):
                                   auto_join=True)
     tag_ids = fields.Many2many('fsm.tag',
                                string='Tags')
-    building = fields.Char(string='Building', size=35)
-    floor = fields.Char(string='Floor', size=35)
-    unit = fields.Char(string='Unit', size=35)
-    room = fields.Char(string='Room', size=35)
     description = fields.Char(string='Description')
-    territory = fields.Char(string='Territory', size=35)
-    branch = fields.Char(string='Branch', size=35)
-    district = fields.Char(string='District', size=35)
-    region = fields.Char(string='Region', size=35)
-    timezone = fields.Char(string='Timezone', size=35)
+    location_id = fields.Many2one('location', string='Location')
+    territory_id = fields.Many2one('territory', string='Territory')
+    branch_id = fields.Many2one('branch', string='Branch')
+    district_id = fields.Many2one('district', string='District')
+    region_id = fields.Many2one('region', string='Region')
+    timezone = fields.Selection(_tz_get, string='Timezone')
 
     @api.model
     def create(self, vals):
         vals.update({'fsm_location': True})
         return super(FSMLocation, self).create(vals)
+
+    @api.onchange('territory_id')
+    def _onchange_territory_id(self):
+        self.branch_id = self.territory_id.branch_id
+
+    @api.onchange('branch_id')
+    def _onchange_branch_id(self):
+        self.district_id = self.branch_id.district_id
+
+    @api.onchange('district_id')
+    def _onchange_district_id(self):
+        self.region_id = self.district_id.region_id
