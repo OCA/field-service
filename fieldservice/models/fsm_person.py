f5387190b2e62112695505bf08f787938f9449f5
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-10-22 13:39:07 -0500

    [FIX] PR comments

--- fieldservice/models/fsm_person.py
+++ fieldservice/models/fsm_person.py
@@ -6,12 +6,11 @@ from odoo import api, fields, models
 
 class FSMPerson(models.Model):
     _name = 'fsm.person'
-    _inherits = {'res.partner': 'partner_id'}
     _description = 'Field Service Person'
 
     partner_id = fields.Many2one('res.partner', string='Related Partner',
                                  required=True, ondelete='restrict',
-                                 auto_join=True)
+                                 delegate=True, auto_join=True)
 
     @api.model
     def create(self, vals):
