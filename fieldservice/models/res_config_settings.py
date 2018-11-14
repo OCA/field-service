d4a3c82a565e3d48b80ee8ea58b9e7a124a7cca1
osi-scampbell <scampbell@ursainfosystem.com>
2018-11-14 14:16:36 -0700

    [ADD] FSM Person, FSM Location, location_hierarchy

--- fieldservice/models/res_config_settings.py
+++ fieldservice/models/res_config_settings.py
@@ -13,6 +13,19 @@ class ResConfigSettings(models.TransientModel):
     group_fsm_vehicle = fields.Boolean(
         string='Manage Vehicles',
         implied_group='fieldservice.group_fsm_vehicle')
-
+    group_fsm_category = fields.Boolean(
+        string='Manage Categories',
+        implied_group='fieldservice.group_fsm_category')
+    group_fsm_tag = fields.Boolean(
+        string='Manage Tags',
+        implied_group='fieldservice.group_fsm_tag')
+    group_fsm_equipment = fields.Boolean(
+        string='Manage Equipment',
+        implied_group='fieldservice.group_fsm_equipment')
+    module_fieldservice_agreement = fields.Boolean(
+        string='Manage Agreements')
+    module_fieldservice_skill = fields.Boolean(
+        string='Manage Skills')
     module_fieldservice_stock = fields.Boolean(
-        sting='Use Odoo Stock Logistics')
+        string='Use Odoo Stock Logistics',
+        implied_group='fieldservice.group_fsm_vehicle')
