05c19e334b1194326879281dafa901fcaee63e46
brian10048 <brian@mcmpest.com>
2018-11-24 15:13:33 -0500

    [FIX] fieldservice_stock
    Travis errors

--- fieldservice_stock/models/fsm_location.py
+++ fieldservice_stock/models/fsm_location.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FSMLocation(models.Model):
