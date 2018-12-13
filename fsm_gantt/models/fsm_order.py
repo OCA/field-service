38ccfdad85fbfa39bc168c84f803af4a3a4c59e5
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-12-12 23:03:22 -0800

    [IMP]12586: adjust fsm.order class from model.models to geo_engine

--- fsm_gantt/models/fsm_order.py
+++ fsm_gantt/models/fsm_order.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, fields, _
+from odoo.addons.base_geoengine import geo_model
 
 
-class FSMOrder(models.Model):
+class FSMOrder(geo_model.GeoModel):
     _inherit = 'fsm.order'
 
     branch_id = fields.Many2one('branch', string='Branch')
