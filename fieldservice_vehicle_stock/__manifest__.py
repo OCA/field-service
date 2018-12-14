6e48e7f27cae6c1a5b314935d31d97f6c67f06d5
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-13 18:57:19 -0600

    [ADD] fieldservice_vehicle_stock

--- fieldservice_vehicle_stock/__manifest__.py
+++ fieldservice_vehicle_stock/__manifest__.py
@@ -2,23 +2,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Stock',
-    'summary': 'Inventory and Stock Operations for Field Services',
-    'version': '11.0.0.0.2',
+    'name': 'Field Service Vehicle - Stock',
+    'summary': 'Inventory and Stock Operations for Field Service with Vehicles',
+    'version': '11.0.0.0.1',
     'category': 'Field Service',
     'author': "Open Source Integrators, "
               "Brian McMaster, "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/field-service',
     'depends': [
-        'fieldservice',
-        'stock',
+        'fieldservice_vehicle',
+        'fieldservice_stock',
     ],
     'data': [
         'data/fsm_stock_data.xml',
-        'views/fsm_location.xml',
-        'views/fsm_order.xml',
-        'views/stock.xml',
+        'views/fsm_vehicle.xml',
     ],
     'installable': True,
     'license': 'AGPL-3',
--- fieldservice_vehicle_stock/models/__init__.py
+++ fieldservice_vehicle_stock/models/__init__.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
-    fsm_location,
+    fsm_vehicle,
     fsm_order,
-    stock,
 )
