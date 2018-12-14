fb0ebfb930d9b8c93bf7f781e038354107f975b6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-14 09:06:17 -0600

    [IMP] Views

--- fieldservice/__manifest__.py
+++ fieldservice/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service',
     'summary': 'Manage Field Service Locations, Persons and Orders',
-    'version': '11.0.0.0.2',
+    'version': '11.0.0.1.0',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/field-service',
