8dea2373721ccc98cd704c4f9032c57a6c6d5d20
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-12-14 02:52:25 -0800

    [IMP]12573: make custom color for fsm stage will reflect to geoengine mapping

--- fieldservice/models/fsm_stage.py
+++ fieldservice/models/fsm_stage.py
@@ -1,8 +1,7 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
-
+from odoo import api, fields, models
 
 AVAILABLE_PRIORITIES = [
     ('0', 'Normal'),
@@ -32,3 +31,19 @@ class FSMStage(models.Model):
     is_closed = fields.Boolean('Is a close stage',
                                help='Services in this stage are considered '
                                     'as closed.')
+    custom_color = fields.Char("Color Code", default="#FFFFFF")
+
+    @api.multi
+    def get_color_information(self):
+        # get stage ids
+        stage_ids = self.search([])
+        color_information_dict = []
+        for stage in stage_ids:
+            color_information_dict.append({
+                'color': stage.custom_color,
+                'field': 'stage_id',
+                'opt': '==',
+                'value': stage.name,
+            })
+        return color_information_dict
+
