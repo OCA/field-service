1cabe8c0dea52e17a4a03746fb13902bb3d34572
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-14 17:17:28 -0600

    [FIX] External ID

--- fieldservice_vehicle/__manifest__.py
+++ fieldservice_vehicle/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service Vehicles',
     'summary': 'manage your field service vehicles',
-    'version': '11.0.0.0.2',
+    'version': '11.0.0.1.0',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/field-service',
