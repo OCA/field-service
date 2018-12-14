70029b224ac24fac0f14ef745fdda6156b531b9e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-13 19:48:34 -0600

    [ADD] Security

--- fieldservice_vehicle/models/fsm_route.py
+++ fieldservice_vehicle/models/fsm_route.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 
 
 class FSMRoute(models.Model):
@@ -12,5 +12,5 @@ class FSMRoute(models.Model):
     _sql_constraints = [
         ('fsm_route_vehicle_date_uniq',
          'unique (vehicle_id, date)',
-         "You cannot create 2 routes for the same vehicle on the same day!"),
+         _("The vehicle is already assigned to another route on that day!")),
     ]
