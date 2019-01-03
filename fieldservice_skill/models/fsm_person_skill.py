99b9c5b81c25a6cdb0dac1497b8a558358a17168
osi-scampbell <scampbell@opensourceintegrators.com>
2019-01-03 13:25:02 -0700

    [IMP] Various

--- fieldservice_skill/models/fsm_person_skill.py
+++ fieldservice_skill/models/fsm_person_skill.py
@@ -8,12 +8,14 @@ class FSMPersonSkill(models.Model):
     _name = 'fsm.person.skill'
     _description = 'Field Service Person Skill'
 
+    LEVEL = [('0', 'Junior'),
+             ('1', 'Intermediate'),
+             ('2', 'Senior'),
+             ('3', 'Expert')]
+
     person_id = fields.Many2one('fsm.person', string="Field Service Person")
     skill_id = fields.Many2one('hr.skill', string="Skill", required=True)
-    level = fields.Selection([('0', 'Junior'),
-                              ('1', 'Intermediate'),
-                              ('2', 'Senior'),
-                              ('3', 'Expert')], string='Level')
+    level = fields.Selection(LEVEL, string='Level', default=LEVEL[0][0])
 
     _sql_constraints = [
         ('person_skill_uniq', 'unique(person_id, skill_id)',
