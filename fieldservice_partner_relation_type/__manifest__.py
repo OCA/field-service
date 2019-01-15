4ed305c9fe70c2522d0b47575e06450d6ada26fb
osi-scampbell <scampbell@opensourceintegrators.com>
2019-01-15 16:44:22 -0700

    [IMP] Authors

--- fieldservice_partner_relation_type/__manifest__.py
+++ fieldservice_partner_relation_type/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Field Service Partner Relations",
     "version": "11.0.1.0.0",
     'category': 'Field Service',
-    'author': "Open Source Integrators, ",
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     "website": "https://github.com/OCA/partner-contact",
     "complexity": "normal",
     "license": "AGPL-3",
