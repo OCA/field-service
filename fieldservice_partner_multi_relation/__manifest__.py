34e53082287effb0edbee60bc9dda27a36e1609d
osi-scampbell <scampbell@opensourceintegrators.com>
2019-01-17 09:27:00 -0700

    [IMP] Fix Readme

---
+++ fieldservice_partner_multi_relation/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2018 - TODAY, Brian McMaster
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Field Service Partner Relations",
+    "version": "11.0.1.0.0",
+    'category': 'Field Service',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    "website": "https://github.com/OCA/partner-contact",
+    "complexity": "normal",
+    "license": "AGPL-3",
+    "depends": [
+        'contacts',
+        'sales_team',
+        'partner_multi_relation'
+    ],
+    "demo": [
+        "data/demo.xml",
+    ]
+
+}
