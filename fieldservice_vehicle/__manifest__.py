56b2aeebb29f5f2794ccbe1046755a828079137d
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-01 19:06:36 -0600

    [FIX] fieldservice_vehicle

--- fieldservice_vehicle/__manifest__.py
+++ fieldservice_vehicle/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service Vehicles',
     'summary': 'manage your field service vehicles',
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.1.1',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/field-service',
