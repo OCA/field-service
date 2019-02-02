dc0d04611ba1c4815796391c00072ac90ba5fce1
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-01 19:06:16 -0600

    [FIX] fieldservice_stock

--- fieldservice_stock/models/fsm_location.py
+++ fieldservice_stock/models/fsm_location.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields
+from odoo import api, fields
 from odoo.addons.base_geoengine import geo_model
 
 
@@ -11,3 +11,9 @@ class FSMLocation(geo_model.GeoModel):
     inventory_location_id = fields.Many2one('stock.location',
                                             string='Inventory Location',
                                             required=True)
+
+    @api.onchange('parent_id')
+    def _onchange_parent_id(self):
+        super(FSMLocation, self)._onchange_parent_id()
+        self.inventory_location_id = \
+            self.parent_id.inventory_location_id or False
