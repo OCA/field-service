128a6da3b8bdaa3b0aee477bf332e84f6c699345
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-08 00:10:04 -0600

    [ADD] Merge fieldservice_equipment_agreement

--- fieldservice_agreement/__manifest__.py
+++ fieldservice_agreement/__manifest__.py
@@ -2,26 +2,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Field Service - Agreements",
-    "summary": "Manage FSM Agreements",
-    "author": "Open Source Integrators, "
-              "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/field-service",
-    "category": "Field Service",
-    "license": "AGPL-3",
-    "version": "11.0.1.0.0",
-    "depends": [
-        "fieldservice",
-        "agreement",
+    'name': 'Field Service - Agreements',
+    'summary': 'Manage FSM Agreements',
+    'author': 'Open Source Integrators, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'category': 'Field Service',
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'fieldservice',
+        'agreement',
     ],
-    "data": [
-        "views/agreement_view.xml",
-        "views/fsm_order_view.xml",
+    'data': [
+        'views/fsm_order_view.xml',
+        'views/fsm_equipment_view.xml',
+        'views/agreement_view.xml',
     ],
-    "development_status": "Beta",
-    "maintainers": [
-        "max3903",
-        "bodedra",
-        "smangukiya",
+    'development_status': 'Beta',
+    'maintainers': [
+        'max3903',
+        'bodedra',
+        'smangukiya',
     ],
 }
