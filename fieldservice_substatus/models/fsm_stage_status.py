aeb29aba33b834a10b8c5fd81dc0c1fabebf4c2c
Bhavesh Odedra <bodedra@ursainfosystems.com>
2019-02-11 12:52:32 +0530

    [FIX] travis-ci

--- fieldservice_substatus/models/fsm_stage_status.py
+++ fieldservice_substatus/models/fsm_stage_status.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators, Brian McMaster
+# Copyright (C) 2019 - TODAY, Open Source Integrators, Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FSMStageStatus(models.Model):
