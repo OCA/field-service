3832801846d4ff351e1974625c74fcb500cb2166
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-03 20:53:20 +0530

    [MIG][WIP][12.0] fieldservice_sale

--- fieldservice_sale/__manifest__.py
+++ fieldservice_sale/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service - Sales',
     'summary': 'Sales',
-    'version': '11.0.0.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/field-service',
@@ -21,4 +21,5 @@
         'wolfhall',
         'max3903',
     ],
+    'installable': True,
 }
