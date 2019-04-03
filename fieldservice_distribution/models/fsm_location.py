c2f55ba1c9fc5a36ab5f78c2684be009a5251d20
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-03 21:15:44 +0530

    [MIG][WIP][12.0] fieldservice_distribution

--- fieldservice_distribution/models/fsm_location.py
+++ fieldservice_distribution/models/fsm_location.py
@@ -1,11 +1,10 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields
-from odoo.addons.base_geoengine import geo_model
+from odoo import api, fields, models
 
 
-class FSMLocation(geo_model.GeoModel):
+class FSMLocation(models.Model):
     _inherit = 'fsm.location'
 
     is_a_distribution = fields.Boolean(string='Is a Distribution')
@@ -19,36 +18,30 @@ class FSMLocation(geo_model.GeoModel):
     @api.multi
     def _compute_distrib_sublocation_ids(self):
         for location in self:
-            for child in location:
-                child_locs = self.env['fsm.location'].search(
-                    [('fsm_parent_id', '=', child.id)])
-                count = self.env['fsm.location'].search_count(
-                    [('fsm_parent_id', '=', child.id),
-                     ('is_a_distribution', '=', True)])
-            if child_locs:
-                for loc in child_locs:
-                    count += loc.distrib_count
-            location.distrib_count = count
+            location.distrib_count = self.env['fsm.location'].search_count(
+                [('fsm_parent_id', 'child_of', location.id), (
+                    'id', '!=', location.id), (
+                    'is_a_distribution', '=', True)])
 
     @api.multi
     def action_view_distrib_sublocation(self):
-        '''
+        """
         This function returns an action that display existing
         distribution sub-locations of a given fsm location id.
         It can either be a in a list or in a form view, if there is only one
         sub-location to show.
-        '''
+        """
         for location in self:
             action = self.env.ref('fieldservice.action_fsm_location').read()[0]
             sublocation = self.get_action_views(0, 0, location)
             sublocation.filtered(lambda r: r.is_a_distribution)
-            if len(sublocation) > 1 or len(sublocation) == 0:
-                action['domain'] = [('id', 'in', sublocation.ids),
-                                    ('is_a_distribution', '=', True)]
-            elif sublocation:
+            if len(sublocation) == 1:
                 action['views'] = [(self.env.
                                     ref('fieldservice.' +
                                         'fsm_location_form_view').id,
                                     'form')]
                 action['res_id'] = sublocation.id
+            else:
+                action['domain'] = [('id', 'in', sublocation.ids),
+                                    ('is_a_distribution', '=', True)]
             return action
