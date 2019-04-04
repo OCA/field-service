22475c299dfaa1e5e5514eac6db3375c9a24ccab
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-04 18:16:00 +0530

    [MIG][WIP][12.0] fieldservice_skill

--- fieldservice_skill/models/fsm_order.py
+++ fieldservice_skill/models/fsm_order.py
@@ -1,12 +1,10 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields
-
-from odoo.addons.base_geoengine import geo_model
+from odoo import api, fields, models
 
 
-class FSMOrder(geo_model.GeoModel):
+class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
     skill_ids = fields.Many2many('hr.skill', string="Required Skills")
