37ad06e7d9ab5e7a6df080747a5892b1b7ce8920
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-04 20:50:10 +0530

    [MIG][WIP][12.0] fieldservice_stock
    [UPD] README.rst

--- fieldservice_stock/models/stock.py
+++ fieldservice_stock/models/stock.py
@@ -4,43 +4,64 @@
 from odoo import api, fields, models
 
 
+class StockRequest(models.Model):
+    _inherit = "stock.request"
+
+    fsm_order_id = fields.Many2one(
+        'fsm.order', string="FSM Order", ondelete='cascade',
+        index=True, copy=False)
+    direction = fields.Selection([('outbound', 'Outbound'),
+                                  ('inbound', 'Inbound')], string='Direction')
+
+    @api.onchange('direction')
+    def onchange_direction(self):
+        """To set route based on direction."""
+        route_ids = False
+        if self.direction == 'outbound':
+            route_ids = self.env['stock.location.route'].search(
+                [('fsm_selectable', '=', True)])
+        if self.direction == 'inbound':
+            route_ids = self.env['stock.location.route'].search(
+                [('fsm_return_selectable', '=', True)])
+        return {'domain': {'route_id': [('id', 'in',
+                                         route_ids and route_ids.ids)]}}
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    def _action_done(self):
+        res = super(StockMoveLine, self)._action_done()
+        for rec in self:
+            for all_rec in rec.move_id.allocation_ids:
+                request = all_rec.stock_request_id
+                if request.state == 'done' and request.fsm_order_id:
+                    request.fsm_order_id.request_stage = 'done'
+        return res
+
+
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    fsm_order_line_id = fields.Many2one('fsm.order.line', 'FSM Order Line')
-    fsm_order_return_line_id = fields.Many2one(
-        'fsm.order.return', 'FSM Order Return Line')
-
     def prepare_equipment_values(self, move_line):
-        return {'name': '%s (%s)' % (move_line.product_id.name,
-                                     move_line.lot_id.name),
-                'product_id': move_line.product_id.id,
-                'lot_id': move_line.lot_id.id}
+        return {'name': '%s (%s)' % (
+            move_line.product_id.name, move_line.lot_id.name),
+            'product_id': move_line.product_id.id,
+            'lot_id': move_line.lot_id.id,
+            'current_location_id':
+            move_line.request_id.fsm_order_id.location_id.id,
+            'current_stock_location_id': move_line.dest_location_id.id}
 
     def _action_done(self):
         res = False
         for rec in self:
             res = super(StockMove, rec)._action_done()
-            for line in res.mapped('fsm_order_line_id').sudo():
-                line.qty_delivered = line._get_delivered_qty()
-            for line in res.mapped('fsm_order_return_line_id').sudo():
-                line.qty_received = line._get_received_qty()
             if rec.picking_code == 'outgoing' and rec.state == 'done':
                 if rec.product_tmpl_id.create_fsm_equipment:
                     for line in rec.move_line_ids:
                         vals = self.prepare_equipment_values(line)
-                        line.lot_id.equipment_id = rec.env['fsm.equipment'].\
-                            create(vals)
-        return res
-
-    @api.multi
-    def write(self, vals):
-        res = super(StockMove, self).write(vals)
-        if 'product_uom_qty' in vals:
-            for move in self:
-                if move.state == 'done':
-                    for line in self.mapped('fsm_order_line_id').sudo():
-                        line.qty_delivered = line._get_delivered_qty()
+                        line.lot_id.fsm_equipment_id = \
+                            rec.env['fsm.equipment'].create(vals)
         return res
 
 
@@ -48,22 +69,6 @@ class ProcurementGroup(models.Model):
     _inherit = 'procurement.group'
 
     fsm_order_id = fields.Many2one('fsm.order', 'Field Service Order')
-
-
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
-
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
-        result = super(ProcurementRule, self)._get_stock_move_values(
-            product_id, product_qty, product_uom,
-            location_id, name, origin, values, group_id)
-        if values.get('fsm_order_line_id', False):
-            result['fsm_order_line_id'] = values['fsm_order_line_id']
-        if values.get('fsm_order_return_line_id', False):
-            result['fsm_order_return_line_id'] = \
-                values['fsm_order_return_line_id']
-        return result
 
 
 class StockPicking(models.Model):
@@ -89,27 +94,15 @@ class StockPickingType(models.Model):
     def _compute_fsm_request(self):
         for ptype in self:
             if ptype.code == 'outgoing':
-                res = self.env['fsm.order'].search([('warehouse_id',
-                                                     '=',
-                                                     ptype.warehouse_id.id),
-                                                    ('inventory_stage', '=',
-                                                     'requested')])
-                for order in res:
-                    for line in order.line_ids:
-                        if line.state == 'requested':
-                            ptype.count_fsm_requests += 1
-                            break
+                res = self.env['fsm.order'].search(
+                    [('request_stage', '=', 'draft'),
+                     ('warehouse_id', '=', ptype.warehouse_id.id)])
+                ptype.count_fsm_requests = len(res)
             if ptype.code == 'incoming':
-                res = self.env['fsm.order'].search([('warehouse_id',
-                                                     '=',
-                                                     ptype.warehouse_id.id),
-                                                    ('inventory_stage', '=',
-                                                     'requested')])
-                for order in res:
-                    for line in order.return_ids:
-                        if line.state == 'requested':
-                            ptype.count_fsm_requests += 1
-                            break
+                res = self.env['fsm.order'].search(
+                    [('request_stage', '=', 'draft'),
+                     ('warehouse_id', '=', ptype.warehouse_id.id)])
+                ptype.count_fsm_requests = len(res)
 
     def get_action_fsm_requests(self):
         return self._get_action('fieldservice_stock.action_stock_fsm_order')
