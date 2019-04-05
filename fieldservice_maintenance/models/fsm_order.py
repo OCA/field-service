f91cdcc496d53839527a3ed952e7eb5aa1686e46
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-05 20:30:17 +0530

    [MIG][WIP][12.0] fieldservice_maintenance

--- fieldservice_maintenance/models/fsm_order.py
+++ fieldservice_maintenance/models/fsm_order.py
@@ -1,11 +1,10 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields
-from odoo.addons.base_geoengine import geo_model
+from odoo import api, fields, models
 
 
-class FSMOrder(geo_model.GeoModel):
+class FSMOrder(models.Model):
     _inherit = 'fsm.order'
     _description = 'Field Service Order Maintenance'
 
@@ -23,7 +22,7 @@ class FSMOrder(geo_model.GeoModel):
                 [('user_id', '=', self.env.uid)])
             if employee_ids:
                 employee_id = employee_ids[0]
-            if order.equipment_id:
+            if order.equipment_id and not order.equipment_id.is_fsm_equipment:
                 equipment = order.equipment_id
                 t = equipment.maintenance_equipment_id and\
                     equipment.maintenance_equipment_id.maintenance_team_id \
