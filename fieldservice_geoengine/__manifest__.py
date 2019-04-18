b10e9a6486fbd8d57d97cd95ecc8976d86bcb8b4
Murtuza Saleh(SerpentCS) <m.saleh.serpentcs@gmail.com>
2019-04-18 23:39:12 +0530

    [ADD][WIP][12.0] fieldservice_geoengine (#170)

---
+++ fieldservice_geoengine/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service Geoengine',
+    'summary': 'Extend field service functionality based on Geoengine',
+    'version': '12.0.1.0.0',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'depends': [
+        'base_geoengine',
+        'fieldservice',
+    ],
+    'data': [
+        'views/fsm_order.xml',
+        'views/fsm_team.xml'
+    ],
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+        'max3903',
+    ],
+}
