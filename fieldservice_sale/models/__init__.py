3b44f234aeda4275ce60508d945bb9b0261ac558
brian10048 <brian@mcmpest.com>
2019-05-11 17:54:52 -0400

    [IMP] fieldservice_sale
    Added ability to link a product with FSM order template and create FSM
    orders upon sale order confirmation
    Added option to install fieldservice_sale module

--- fieldservice_sale/models/__init__.py
+++ fieldservice_sale/models/__init__.py
@@ -2,3 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import fsm_location
+from . import product
+from . import sale_order
+from . import fsm_order
---
+++ fieldservice_sale/models/product.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2019 - TODAY, Brian McMaster, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    service_type = fields.Selection(selection_add=[
+        ('field', 'Field Service Orders'),
+    ])
+    field_service_tracking = fields.Selection([
+        ('no', 'Don\'t create order'),
+        ('order', 'Create a single order'),
+    ], string="Field Service Tracking", default="no",
+        help="""On Sales order confirmation, this product can generate a field
+                service order.""")
+    fsm_order_template_id = fields.Many2one(
+        'fsm.template', 'Field Service Order Template',
+        help="Select the field service order template to be created")
+
+    @api.onchange('field_service_tracking')
+    def _onchange_field_service_tracking(self):
+        if self.field_service_tracking != 'order':
+            self.fsm_order_template_id = False
