ce3b30f9bf67c0f508f2cfd06ec0eb0ca34f4dd0
brian10048 <brian@mcmpest.com>
2019-05-13 19:19:11 -0400

    [IMP] fieldservice_sale
    Added support for selling recurring orders

--- fieldservice_sale/models/product.py
+++ fieldservice_sale/models/product.py
@@ -13,14 +13,20 @@ class ProductTemplate(models.Model):
     field_service_tracking = fields.Selection([
         ('no', 'Don\'t create order'),
         ('order', 'Create a single order'),
+        ('recurring', 'Create a recurring order')
     ], string="Field Service Tracking", default="no",
         help="""On Sales order confirmation, this product can generate a field
-                service order.""")
+                service order or field service recurring order.""")
     fsm_order_template_id = fields.Many2one(
         'fsm.template', 'Field Service Order Template',
         help="Select the field service order template to be created")
+    fsm_recurring_template_id = fields.Many2one(
+        'fsm.recurring', 'Field Service Recurring Template',
+        help="Select a field service recurring order template to be created")
 
     @api.onchange('field_service_tracking')
     def _onchange_field_service_tracking(self):
         if self.field_service_tracking != 'order':
             self.fsm_order_template_id = False
+        elif self.field_service_tracking != 'recurring':
+            self.fsm_recurring_template_id = False
