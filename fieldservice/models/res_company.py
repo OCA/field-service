9480c919de38cddd83c2a715142cb01c21bf34c8
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-05-20 12:54:41 -0500

    [IMP] fieldservice: Add an option to auto-fill workers on location

---
+++ fieldservice/models/res_company.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    auto_populate_persons_on_location = fields.Boolean(
+        string='Auto-populate Workers on Location based on Territory')
