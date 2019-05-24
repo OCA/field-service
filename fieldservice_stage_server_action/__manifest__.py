22323d620885d61a4a5fe74519a459d28aa0b8da
scampbell <scampbell@opensourceintegrators.com>
2019-05-24 07:38:29 -0700

    [WIP][ADD] fsm_server_action
    [ADD] Templates
    
    [WIP] Testing
    
    [IMP] FSM Emails
    
    [ADD] FSM Stage Server Action
    
    [IMP] ir.server.action in pavlov_media
    
    [IMP] Remove dependency conflict
    
    [IMP] Cite OCA manifest
    
    [IMP] Remove Dependency
    
    [IMP] Auto Config for User
    
    [IMP] Small changes
    
    [FIX] fieldservice (PM #515 and #577) (#230)
    
    [IMP] fieldservice: Add notes on location (PM #596) (#231)
    
    [FIX] fieldservice_agreement (#232)
    
    [ADD] fieldservice_stage_server_action

---
+++ fieldservice_stage_server_action/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'FSM Stage Server Action',
+    'summary': 'Add Server Actions based on FSM Stage',
+    'version': '12.0.1.0.0',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    'website': 'https://github.com/ursais/osi-addons',
+    'depends': [
+        'fieldservice',
+        'fieldservice_substatus',
+        'base_automation'
+    ],
+    'data': [
+        'data/ir_server_action.xml',
+        'data/fsm_stage.xml',
+        'data/base_automation.xml',
+        'views/fsm_stage.xml'
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+        'max3903',
+        'osi-scampbell'
+    ]
+}
--- fieldservice_stage_server_action/models/__init__.py
+++ fieldservice_stage_server_action/models/__init__.py
@@ -1,9 +1,6 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
-    agreement,
-    fsm_equipment,
-    fsm_location,
-    fsm_order,
-    fsm_person,
+    fsm_stage,
 )
