9be6dbf915a16bf2158802b29d9bc0ea8fecd560
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-05 14:12:11 -0500

    [FIX] fieldservice_repair: formatting

--- fieldservice_repair/models/fsm_order.py
+++ fieldservice_repair/models/fsm_order.py
@@ -1,16 +1,16 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import fields, models
+# from odoo import api, fields, models, _
+# from odoo.exceptions import ValidationError
 
 
 class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
     type = fields.Selection(selection_add=[('repair', 'Repair')])
-    repair_id = fields.Many2one(
-        'repair.order', 'Repair Order')
+    repair_id = fields.Many2one('repair.order', string='Repair Order')
 
     # @api.model
     # def create(self, vals):
