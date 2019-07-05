0c595f121a20e546bd86a8a0faff759a07580034
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-05 16:15:42 -0500

    [FIX] fieldservice_account: Bump version and provide migration script

---
+++ fieldservice_account/migrations/12.0.2.0.0/pre-migration.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def migrate(env, version):
+    if not version:
+        return
+
+    env.execute("UPDATE fsm_order SET bill_to = 'location' "
+                "WHERE bill_to IS NULL;")
