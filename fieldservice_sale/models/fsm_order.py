457c5976067120d497a52e3d56d122e634d4daf6
hparfr <raph@hpar.fr>
2019-07-15 16:38:28 +0200

    Finish migration to v12 (forward port from v11)

--- fieldservice_sale/models/fsm_order.py
+++ fieldservice_sale/models/fsm_order.py
@@ -1,12 +1,10 @@
 # Copyright (C) 2019 - TODAY, Brian McMaster, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, _
-
-from odoo.addons.base_geoengine import geo_model
+from odoo import fields, models, _
 
 
-class FSMOrder(geo_model.GeoModel):
+class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
     sale_line_id = fields.Many2one('sale.order.line')
