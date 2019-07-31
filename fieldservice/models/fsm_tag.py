11a77aafe45a966309b53c28e6e09c7869f8fa06
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-31 17:59:39 -0500

    [IMP] fieldservice: Multi-company support

--- fieldservice/models/fsm_tag.py
+++ fieldservice/models/fsm_tag.py
@@ -12,6 +12,10 @@ class FSMTag(models.Model):
     parent_id = fields.Many2one('fsm.tag', string='Parent')
     color = fields.Integer('Color Index', default=10)
     full_name = fields.Char(string='Full Name', compute='_compute_full_name')
+    company_id = fields.Many2one(
+        'res.company', string='Company', required=True, index=True,
+        default=lambda self: self.env.user.company_id,
+        help="Company related to this tag")
 
     _sql_constraints = [
         ('name_uniq', 'unique (name)', "Tag name already exists!"),
