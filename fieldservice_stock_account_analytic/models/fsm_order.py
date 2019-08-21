9ec7c16ef12fffc65f119c80cd362f2777edd4ad
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-08-21 18:23:20 -0500

    [FIX] fieldservice_stock_account_analytic: PEP8 errors

--- fieldservice_stock_account_analytic/models/fsm_order.py
+++ fieldservice_stock_account_analytic/models/fsm_order.py
@@ -1,8 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, _
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class FSMOrder(models.Model):
