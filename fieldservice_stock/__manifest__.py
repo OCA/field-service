41d4ac1f35eb7cbdbaeb10c5fc4d15ec042f7f8e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-08-20 19:06:55 -0500

    [IMP] fieldservice_stock

--- fieldservice_stock/__manifest__.py
+++ fieldservice_stock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service - Stock',
     'summary': 'Inventory and Stock Operations for Field Services',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Field Service',
     'author': "Open Source Integrators, "
               "Brian McMaster, "
