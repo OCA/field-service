3896ae5ea081c5191af6e8f6549a073dbd975d74
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-08-27 15:17:58 -0600

    [FIX] LINT Error
    fixed lint error (missing OCA in authors)

--- fieldservice_project/__manifest__.py
+++ fieldservice_project/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Create field service orders from a project or project task',
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Pavlov Media',
+    'author': 'Pavlov Media, Odoo Community Association (OCA)',
     'category': 'Project',
     'website': 'https://www.pavlovmedia.com',
     'depends': [
