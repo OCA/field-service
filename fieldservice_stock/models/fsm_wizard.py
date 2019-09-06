a4d9f6278f4d9cccbada89e6ffd3f977dbd44207
hparfr <raph@hpar.fr>
2019-09-06 17:37:25 +0200

    Fix customer_id

--- fieldservice_stock/models/fsm_wizard.py
+++ fieldservice_stock/models/fsm_wizard.py
@@ -1,25 +1,13 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, _
-from odoo.exceptions import UserError
+from odoo import models
 
 
 class FSMWizard(models.TransientModel):
     _inherit = 'fsm.wizard'
 
-    def action_convert_location(self, partner):
-        res = self.env['fsm.location'].search_count(
-            [('partner_id', '=', partner.id)])
-        if res == 0:
-            vals = {'partner_id': partner.id,
-                    'owner_id': partner.id,
-                    'customer_id': partner.id,
-                    'inventory_location_id':
-                    partner.property_stock_customer.id}
-            self.env['fsm.location'].create(vals)
-            partner.write({'fsm_location': True})
-        else:
-            raise UserError(_('A Field Service Location related to that'
-                              ' partner already exists.'))
+    def _prepare_fsm_location(self, partner):
+        res = super(FSMWizard)._prepare_fsm_location(partner)
+        res['inventory_location_id'] = partner.property_stock_customer.id
         return res
