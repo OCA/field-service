d556cf92641a34f893446d77fd0ee5fca7f1410f
hparfr <raph@hpar.fr>
2019-09-06 17:30:06 +0200

    Add customer_id (moved from base module)

--- fieldservice_account/__manifest__.py
+++ fieldservice_account/__manifest__.py
@@ -19,12 +19,17 @@
         'data/time_products.xml',
         'data/ir_rule.xml',
         'security/ir.model.access.csv',
+        'report/fsm_order_report_template.xml',
         'views/account.xml',
         'views/fsm_location.xml',
         'views/fsm_order.xml',
         'views/fsm_person.xml',
+        'views/fsm_route.xml',
         'views/account_invoice_view.xml',
         'views/hr_timesheet.xml'
+    ],
+    'demo': [
+        'demo/fsm_location.xml',
     ],
     'license': 'AGPL-3',
     'development_status': 'Beta',
