63bde7f14d9b172712a2d28853b8690b149a3a70
hparfr <raph@hpar.fr>
2019-09-06 10:55:33 +0200

    Remove most of the stages and its validation logic

--- fieldservice/models/fsm_order.py
+++ fieldservice/models/fsm_order.py
@@ -225,52 +225,7 @@ class FSMOrder(models.Model):
                     timedelta(hours=self.scheduled_duration)
                 vals['scheduled_date_end'] = str(date_to_with_delta)
 
-    def action_confirm(self):
-        return self.write({'stage_id': self.env.ref(
-            'fieldservice.fsm_stage_confirmed').id})
-
-    def action_request(self):
-        if not self.person_ids:
-            raise ValidationError(_("Cannot move to Requested " +
-                                    "until 'Request Workers' is filled in"))
-        return self.write({'stage_id': self.env.ref(
-            'fieldservice.fsm_stage_requested').id})
-
-    def action_assign(self):
-        if self.person_id:
-            return self.write({'stage_id': self.env.ref(
-                'fieldservice.fsm_stage_assigned').id})
-        else:
-            raise ValidationError(_("Cannot move to Assigned " +
-                                    "until 'Assigned To' is filled in"))
-
-    def action_schedule(self):
-        if self.scheduled_date_start and self.person_id:
-            return self.write({'stage_id': self.env.ref(
-                'fieldservice.fsm_stage_scheduled').id})
-        else:
-            raise ValidationError(_("Cannot move to Scheduled " +
-                                    "until both 'Assigned To' and " +
-                                    "'Scheduled Start Date' are filled in"))
-
-    def action_enroute(self):
-        return self.write({'stage_id': self.env.ref(
-            'fieldservice.fsm_stage_enroute').id})
-
-    def action_start(self):
-        if not self.date_start:
-            raise ValidationError(_("Cannot move to Start " +
-                                    "until 'Actual Start' is filled in"))
-        return self.write({'stage_id': self.env.ref(
-            'fieldservice.fsm_stage_started').id})
-
     def action_complete(self):
-        if not self.date_end:
-            raise ValidationError(_("Cannot move to Complete " +
-                                    "until 'Actual End' is filled in"))
-        if not self.resolution:
-            raise ValidationError(_("Cannot move to Complete " +
-                                    "until 'Resolution' is filled in"))
         return self.write({'stage_id': self.env.ref(
             'fieldservice.fsm_stage_completed').id})
 
