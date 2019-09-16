6e6da2c1e5b1a6abc11b04163b3d7f4554222628
hparfr <raph@hpar.fr>
2019-09-16 10:36:05 +0200

    Add fieldservice contract

---
+++ fieldservice_contract/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2019 - Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service - Contracts',
+    'summary': 'Manage FSM Contracts',
+    'author': 'Akretion, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'category': 'Field Service',
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'contract',
+        'fieldservice_sale',
+        'fieldservice_recurring',
+    ],
+    'data': [
+        'views/contract.xml',
+        'views/contract_line.xml',
+        'views/fsm_recurring.xml',
+        'views/fsm_order.xml',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': [
+        'hparfr',
+    ],
+}
---
+++ fieldservice_contract/models/contract.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Akretion <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ContractContract(models.Model):
+    _inherit = 'contract.contract'
+
+    fsm_location_id = fields.Many2one(
+        string='FSM Location',
+        comodel_name='fsm.location',
+        inverse_name='contract_ids',
+        copy=True,
+    )
+
+    @api.multi
+    def action_view_fsm_recurring(self):
+        fsm_recurrings = self.contract_line_ids.mapped('fsm_recurring_id')
+        action = self.env.ref(
+            'fieldservice_recurring.action_fsm_recurring').read()[0]
+        if len(fsm_recurrings) > 1:
+            action['domain'] = [('id', 'in', fsm_recurrings.ids)]
+        elif len(fsm_recurrings) == 1:
+            action['views'] = [
+                (self.env.ref(
+                    'fieldservice_recurring.fsm_recurring_form_view').id,
+                    'form')]
+            action['res_id'] = fsm_recurrings.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
+
+    @api.multi
+    def action_view_fsm_order(self):
+        # fetch all orders:
+        #    - created directly
+        #    - created by recurring
+        line_ids = self.contract_line_ids.ids
+        action = self.env.ref(
+            'fieldservice.action_fsm_dash_order').read()[0]
+        action['domain'] = [('contract_line_id', 'in', line_ids)]
+        return action
