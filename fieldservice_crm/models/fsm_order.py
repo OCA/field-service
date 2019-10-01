98a8cbf273d92f3a56a99d2c276f70f14e199857
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-10-01 15:37:32 -0500

    [Add] fieldservice_crm
    Adding new module named fieldservice_crm that connects CRM and Field Service Orders. This can be useful in the event where people need to go onsite to evaluate a sales opportunity before quoting.

---
+++ fieldservice_crm/models/fsm_order.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019 - TODAY, Patrick Wilson
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FSMOrder(models.Model):
+    _inherit = 'fsm.order'
+
+    opportunity_id = fields.Many2one('crm.lead', string='Opportunity',
+                                     track_visibility='onchange')
