f827a0907185dc10b2d05da0b9fc6450453360dc
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-09 09:40:22 -0500

    [REN] default_flow --> isp_flow

---
+++ fieldservice_isp_flow/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019 - Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service - Flow for ISP',
+    'summary': 'FSM Stages for Internet Service Providers',
+    'version': '12.0.1.2.0',
+    'category': 'Field Service',
+    'author':
+        'Open Source Integrators,'
+        ' Akretion, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'depends': [
+        'fieldservice',
+    ],
+    'data': [
+        'data/fsm_stage.xml',
+        'views/fsm_order.xml',
+    ],
+    'application': False,
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+}
