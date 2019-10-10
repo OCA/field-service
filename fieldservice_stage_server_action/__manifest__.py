149ea0b28d61bfbd609079b30864c87dc9054061
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-10 14:00:21 -0500

    [FIX] fieldservice_stage_server_action

--- fieldservice_stage_server_action/__manifest__.py
+++ fieldservice_stage_server_action/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2019 Open Source Integrators
 # # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'FSM Stage Server Action',
