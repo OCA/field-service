92387f8b49f6e959e1309bd235ad8f74e98fbd4f
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-11 11:54:01 -0500

    [FIX] fieldservice_account

--- fieldservice_account/__manifest__.py
+++ fieldservice_account/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2018 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Field Service - Accounting',
-    'summary': 'Track employee time and invoice for Field Service Orders',
+    'summary': 'Track employee time and invoice Field Service orders',
     'version': '12.0.2.0.1',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
@@ -33,7 +33,6 @@
     ],
     'license': 'AGPL-3',
     'development_status': 'Beta',
-    'maintainers': [
-        'osimallen',
-    ],
+    'maintainers': ['osimallen'],
+    'pre_init_hook': 'pre_init_hook',
 }
