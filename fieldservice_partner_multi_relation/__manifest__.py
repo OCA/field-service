f3a606f806bf9c9fc8f2262c2ba6b03f6b117cb8
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-11 12:32:38 -0500

    [IMP] fieldservice_partner_multi_relation

--- fieldservice_partner_multi_relation/__manifest__.py
+++ fieldservice_partner_multi_relation/__manifest__.py
@@ -2,23 +2,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Field Service Partner Relations",
+    "summary": "Manage relations between contacts, companies and locations",
     "version": "12.0.1.0.0",
-    'category': 'Field Service',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    "category": "Field Service",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "complexity": "normal",
     "license": "AGPL-3",
     "depends": [
-        'partner_multi_relation',
-        'fieldservice'
+        "partner_multi_relation",
+        "fieldservice"
     ],
     "data": [
-        'views/fsm_location.xml',
-        'views/menu.xml'
+        "views/fsm_location.xml",
+        "views/menu.xml"
     ],
     "demo": [
         "data/demo.xml",
     ],
-    'development_status': 'Beta',
-    'maintainers': ['max3903'],
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
 }
