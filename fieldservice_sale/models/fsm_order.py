c366ade90504babe554d246202642dee03596856
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-16 08:48:31 -0500

    [IMP] fieldservice_sale: Remove the recurring part

--- fieldservice_sale/models/fsm_order.py
+++ fieldservice_sale/models/fsm_order.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2019 - TODAY, Brian McMaster, Open Source Integrators
+# Copyright (C) 2019 Brian McMaster
+# Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models, _
