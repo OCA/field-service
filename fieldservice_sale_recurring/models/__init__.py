319dfd86ac1e37082027c38f6173c100de5815ec
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-16 08:49:01 -0500

    [ADD] fieldservice_sale_recurring

---
+++ fieldservice_sale_recurring/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2018 Brian McMaster
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    product_template,
+    sale_order,
+    sale_order_line,
+    fsm_recurring,
+)
