5cbfb71515ed77f0d2d97735af657c265ec29fb7
Bhavesh Odedra <bodedra@ursainfosystems.com>
2019-10-19 17:02:16 +0530

    [FIX] test_flake8

--- fieldservice_maintenance/models/fsm_equipment.py
+++ fieldservice_maintenance/models/fsm_equipment.py
@@ -15,13 +15,11 @@ class FSMEquipment(models.Model):
     @api.model
     def create(self, vals):
         lot_id = vals.get('lot_id', False)
-        maintenance_equipment_id = self.env['maintenance.equipment'].create({
-            'name': vals.get('name'),
-            'equipment_assign_to': 'other',
-            'is_fsm_equipment': True,
-            'note': vals.get('notes', False),
-            'serial_no': lot_id and
-                         self.env['stock.production.lot'].browse(lot_id).name})
+        maintenance_equipment_id = self.env['maintenance.equipment'].create(
+            {'name': vals.get('name'), 'equipment_assign_to': 'other',
+             'is_fsm_equipment': True, 'note': vals.get('notes', False),
+             'serial_no': lot_id and self.env['stock.production.lot'].browse(
+                 lot_id).name})
         if maintenance_equipment_id:
             vals.update({
                 'maintenance_equipment_id': maintenance_equipment_id.id})
