59ae4d3858debc1030fb00569af28a392ee452bf
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-11-07 08:07:26 -0600

    [FIX] fieldservice_project: CSS download failure (#385)

--- fieldservice_project/__manifest__.py
+++ fieldservice_project/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service - Project',
     'summary': 'Create field service orders from a project or project task',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'license': 'AGPL-3',
     'author': 'Pavlov Media, Odoo Community Association (OCA)',
     'category': 'Project',
