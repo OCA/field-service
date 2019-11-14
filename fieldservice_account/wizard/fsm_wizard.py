048de44a242d3235148d97feb24c90cc972832c7
scampbell <scampbell@opensourceintegrators.com>
2019-11-14 10:13:18 -0800

    [IMP] Fix Convert Error

--- fieldservice_account/wizard/fsm_wizard.py
+++ fieldservice_account/wizard/fsm_wizard.py
@@ -12,6 +12,6 @@ class FSMWizard(models.TransientModel):
     _inherit = 'fsm.wizard'
 
     def _prepare_fsm_location(self, partner):
-        res = super(FSMWizard)._prepare_fsm_location(partner)
+        res = super()._prepare_fsm_location(partner)
         res['owner_id'] = partner.id
         return res
