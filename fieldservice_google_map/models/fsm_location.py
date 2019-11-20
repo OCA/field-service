dad155667851b802f54667d2b45521ae97e5069c
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-11-20 20:23:41 +0530

    [WIP][12.0][ADD] fieldservice_google_map

---
+++ fieldservice_google_map/models/fsm_location.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FSMLocation(models.Model):
+    _inherit = 'fsm.location'
+
+    marker_color = fields.Char(related='stage_id.custom_color',
+                               string='Marker Color')
---
+++ fieldservice_google_map/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module displays map views on the order and location using Google Map View module.
