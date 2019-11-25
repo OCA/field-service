4f7370405ec27697d02e7a8da8765617177917cf
Steve Campbell <scampbell@opensourceintegrators.com>
2019-11-25 14:38:49 -0700

    [FIX] fieldservice_account: Extend FSM Account Wizard (#404)

--- fieldservice_account/wizard/fsm_wizard.py
+++ fieldservice_account/wizard/fsm_wizard.py
@@ -1,17 +1,13 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2018 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import models
 
 
 class FSMWizard(models.TransientModel):
-    """
-        A wizard to convert a res.partner record to a fsm.person or
-         fsm.location
-    """
     _inherit = 'fsm.wizard'
 
     def _prepare_fsm_location(self, partner):
         res = super()._prepare_fsm_location(partner)
+        res['customer_id'] = partner.id
         res['owner_id'] = partner.id
         return res
