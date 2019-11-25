453b0de544da1874ede30d133291b3f935b12856
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-11-25 11:30:31 -0600

    [IMP] fieldservice_sale_recurring

--- fieldservice_sale_recurring/models/sale_order_line.py
+++ fieldservice_sale_recurring/models/sale_order_line.py
@@ -21,12 +21,13 @@ class SaleOrderLine(models.Model):
         return {
             'customer_id': self.order_id.partner_id.id,
             'location_id': self.order_id.fsm_location_id.id,
-            'start_date': self.order_id.date_fsm_request,
+            'start_date': self.order_id.expected_date,
             'fsm_recurring_template_id': template.id,
             'description': note,
             'max_orders': template.max_orders,
             'fsm_frequency_set_id': template.fsm_frequency_set_id.id,
             'fsm_order_template_id': template.fsm_order_template_id.id,
+            'sale_id': self.order_id.id,
             'sale_line_id': self.id,
             'company_id': self.company_id.id,
         }
