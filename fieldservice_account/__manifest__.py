0e4b99f4032a8fafbf8fbd47688f2fccac1fa163
brian10048 <brian@mcmpest.com>
2019-11-26 16:59:14 -0500

    [IMP] fieldservice_account: Simplify module
    - This change removes most of the invoicing logic from fieldservice_account and puts it into its own module

--- fieldservice_account/__manifest__.py
+++ fieldservice_account/__manifest__.py
@@ -3,30 +3,23 @@
 
 {
     'name': 'Field Service - Accounting',
-    'summary': 'Track employee time and invoice Field Service orders',
+    'summary': 'Track invoices linked to Field Service orders',
     'version': '12.0.2.0.1',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/field-service',
     'depends': [
         'fieldservice',
-        'hr_timesheet',
-        'analytic',
         'account',
-        'product',
     ],
     'data': [
-        'data/time_products.xml',
-        'data/ir_rule.xml',
         'security/ir.model.access.csv',
         'report/fsm_order_report_template.xml',
-        'views/account.xml',
         'views/fsm_location.xml',
         'views/fsm_order.xml',
         'views/fsm_person.xml',
         'views/fsm_route.xml',
         'views/account_invoice_view.xml',
-        'views/hr_timesheet.xml'
     ],
     'demo': [
         'demo/fsm_location.xml',
