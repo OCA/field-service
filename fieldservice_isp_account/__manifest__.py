6c349b045106fbfe4614969f5362d7ebad652c0d
brian10048 <brian@mcmpest.com>
2019-11-26 16:59:14 -0500

    [IMP] fieldservice_account: Simplify module
    - This change removes most of the invoicing logic from fieldservice_account and puts it into its own module

---
+++ fieldservice_isp_account/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service - ISP Accounting',
+    'summary': """Invoice Field Service orders based on employee time
+                  or contractor costs""",
+    'version': '12.0.2.0.1',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'depends': [
+        'fieldservice_account_analytic',
+        'hr_timesheet',
+    ],
+    'data': [
+        'data/time_products.xml',
+        'views/account.xml',
+        'views/fsm_order.xml',
+        'views/fsm_person.xml',
+        'views/hr_timesheet.xml',
+    ],
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+    'maintainers': ['osimallen'],
+}
