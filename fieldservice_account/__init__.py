c431d5a86116a39030344713de8d3d7e04640ac1
hparfr <raph@hpar.fr>
2019-11-27 15:30:48 +0100

    Clean up customer_id (billing), cost and change link between account_invoice* and fsm_order

--- fieldservice_account/__init__.py
+++ fieldservice_account/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2018 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import models
-from . import wizard
 from .init_hook import pre_init_hook
