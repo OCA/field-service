ea01739ef95950d535ec8e0d09fc7cadb6d185d9
brian10048 <brian@mcmpest.com>
2019-12-06 09:59:24 -0500

    [FIX] fieldservice_sale
    Consumable and Storable product types were not generating field services

--- fieldservice_sale/models/sale_order.py
+++ fieldservice_sale/models/sale_order.py
@@ -18,7 +18,7 @@ class SaleOrder(models.Model):
         string='Field Service Orders', compute='_compute_fsm_order_ids')
 
     @api.multi
-    @api.depends('order_line.product_id')
+    @api.depends('order_line')
     def _compute_fsm_order_ids(self):
         for order in self:
             orders = self.env['fsm.order']
@@ -119,9 +119,9 @@ class SaleOrder(models.Model):
         return result
 
     @api.multi
-    def action_confirm(self):
+    def _action_confirm(self):
         """ On SO confirmation, some lines generate field service orders. """
-        result = super(SaleOrder, self).action_confirm()
+        result = super(SaleOrder, self)._action_confirm()
         self.order_line._field_service_generation()
         return result
 
