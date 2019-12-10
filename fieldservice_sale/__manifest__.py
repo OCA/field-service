bdf28de6c490d12a02f9b92ba7c21cb03c953959
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-10 09:09:20 -0600

    [FIX] fieldservice_sale: Missing the template info

--- fieldservice_sale/__manifest__.py
+++ fieldservice_sale/__manifest__.py
@@ -18,6 +18,7 @@
         'views/fsm_order.xml',
         'views/product_template.xml',
         'views/sale_order.xml',
+        'views/res_config_settings.xml',
     ],
     'license': 'AGPL-3',
     'development_status': 'Beta',
