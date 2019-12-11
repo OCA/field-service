9fe5635527c6947458818f0607e703817adf0cb5
brian10048 <brian@mcmpest.com>
2019-12-11 12:49:25 -0500

    [IMP] fieldservice_sale: Add test

---
+++ fieldservice_sale/tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2019 Brian McMaster <brian@mcmpest.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    test_fsm_sale_common,
+    test_fsm_sale_order,
+)
