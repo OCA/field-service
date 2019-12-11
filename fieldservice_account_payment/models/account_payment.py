fa5d48faf4d99d9ffed3d716c4da02e3e2806719
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-12-11 20:26:25 +0530

    [WIP][12.0][ADD] fieldservice_account_payment

---
+++ fieldservice_account_payment/models/account_payment.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    fsm_order_id = fields.Many2one('fsm.order', 'FSM Order')
