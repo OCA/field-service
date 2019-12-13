e93a8d0329839d2753524d0a8a07e59bbf4d4786
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2019-12-13 17:27:11 +0530

    [ADD] Added a fieldservice_route v12

---
+++ fieldservice_route/models/__init__.py
@@ -0,0 +1,8 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import fsm_stage
+from . import fsm_route_day
+from . import fsm_route
+from . import fsm_location
+from . import fsm_route_dayroute
+from . import fsm_order
