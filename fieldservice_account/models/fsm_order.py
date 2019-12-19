0b2acdfe3739fa51bbe2fe6da7f6463a9d55d331
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-19 17:03:06 -0600

    [FIX] fieldservice_account
    # Conflicts:
    #	fieldservice_account/models/account_invoice.py

--- fieldservice_account/models/fsm_order.py
+++ fieldservice_account/models/fsm_order.py
@@ -1,23 +1,26 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2018 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
-    invoice_line_id = fields.Many2one(
-        comodel_name="account.invoice.line",
-        readonly=True,
-        copy=False,
-    )
+    invoice_ids = fields.Many2many(
+        'account.invoice', 'fsm_order_account_invoice_rel',
+        'fsm_order_id', 'invoice_id', string='Invoices/Bills')
+    invoice_count = fields.Integer(
+        string='Invoice Count',
+        compute='_compute_account_invoice_count', readonly=True)
 
-    invoice_id = fields.Many2one(
-        related="invoice_line_id.invoice_id",
-        readonly=True,
-    )
-    invoice_ids = fields.One2many('account.invoice',
-                                  'fsm_order_id',
-                                  'Invoices'
-                                  )
+    @api.depends('invoice_ids')
+    def _compute_account_invoice_count(self):
+        for order in self:
+            order.invoice_count = len(order.invoice_ids)
+
+    @api.multi
+    def action_view_invoices(self):
+        action = self.env.ref(
+            'account.action_account_invoice').read()[0]
+        action['domain'] = [('fsm_order_ids', 'in', self.id)]
+        return action
