6352e7f0b1ff6ff68516c11dff8e091cffc7a0c1
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-20 12:55:38 -0600

    [FIX] fieldservice_account_payment

--- fieldservice_account_payment/models/fsm_order.py
+++ fieldservice_account_payment/models/fsm_order.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models, _
 
@@ -6,8 +7,9 @@ from odoo import api, fields, models, _
 class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
-    payment_ids = fields.One2many('account.payment', 'fsm_order_id',
-                                  string='Payments')
+    payment_ids = fields.Many2many(
+        'account.payment', 'fsm_order_account_payment',
+        'fsm_order_id', 'payment_id', string='Payments')
     payment_count = fields.Integer(
         string='Payment Count',
         compute='_compute_account_payment_count', readonly=True)
@@ -21,5 +23,5 @@ class FSMOrder(models.Model):
     def action_view_payments(self):
         action = self.env.ref(
             'account.action_account_payment').read()[0]
-        action['domain'] = [('fsm_order_id', '=', self.id)]
+        action['domain'] = [('fsm_order_ids', 'in', self.id)]
         return action
