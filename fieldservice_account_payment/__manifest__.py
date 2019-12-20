fc13706920c5e51984dce71f5e921e276ae7dd14
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-20 12:55:38 -0600

    [FIX] fieldservice_account_payment

--- fieldservice_account_payment/__manifest__.py
+++ fieldservice_account_payment/__manifest__.py
@@ -1,10 +1,9 @@
 # Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     'name': 'Field Service - Accounting Payment',
-    'summary': 'This module allows the worker to accept payments during'
-    ' his work at the location.',
+    'summary': 'Allow workers to collect payments from the order.',
     'version': '12.0.1.0.0',
     'category': 'Field Service',
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
@@ -13,12 +12,11 @@
         'fieldservice_account',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/fsm_order.xml',
-        'views/account_payment_view.xml',
+        'views/account_payment.xml',
     ],
     'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': [
-        'max3903',
-    ],
+    'development_status': 'Production/Stable',
+    'maintainers': ['max3903'],
 }
