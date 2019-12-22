15fdc4ec1f48da8f0a531e9c54435d5b2beae779
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-22 14:15:50 -0600

    [FIX] fieldservice_sale

--- fieldservice_sale/models/sale_order.py
+++ fieldservice_sale/models/sale_order.py
@@ -18,8 +18,8 @@ class SaleOrder(models.Model):
         compute="_compute_fsm_order_ids",
         string="Field Service orders associated to this sale",
     )
-    fsm_order_count = fields.Float(
-        string="Field Service Orders", compute="_compute_fsm_order_ids"
+    fsm_order_count = fields.Integer(
+        string="FSM Orders", compute="_compute_fsm_order_ids"
     )
 
     @api.multi
