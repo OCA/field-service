ee85c5040be7a98f47de708e02407bbb394df0e0
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-12-23 17:50:16 +0530

    [IMP] Improved code

--- fieldservice_account_payment/tests/__init__.py
+++ fieldservice_account_payment/tests/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import test_fsm_account
+from . import test_fsm_account_payment
