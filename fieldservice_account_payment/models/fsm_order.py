aeef9f69a2c120dd8c8bde8d63e6feed0d1cd8ff
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-12-23 17:50:16 +0530

    [IMP] Improved code

--- fieldservice_account_payment/models/fsm_order.py
+++ fieldservice_account_payment/models/fsm_order.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class FSMOrder(models.Model):
@@ -28,5 +28,5 @@ class FSMOrder(models.Model):
             action['views'] = \
                 [(self.env.ref('account.view_account_payment_form').id,
                   'form')]
-            action['res_id'] = self.payment_ids[0]
+            action['res_id'] = self.payment_ids[0].id
         return action
