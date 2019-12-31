2a11333349350312d0957c4d652ee38ae06605fb
scampbell <scampbell@opensourceintegrators.com>
2019-12-31 09:32:09 -0800

    [ADD] fieldservice_activity
    [IMP] Typo
    
    [IMP] Form/Template Change
    
    [IMP] Show/Hide Buttons
    
    [IMP] PyLint
    
    [IMP] Remove Commented Code
    
    [IMP] Lint

---
+++ fieldservice_activity/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import fsm_activity
+from . import fsm_order
+from . import fsm_template
