455169f9fd41f7985415fa29b212325ecc6f588d
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-01-02 09:54:07 -0600

    [IMP] fieldservice_vehicle_stock: Add putaway strategy for vehicles

--- fieldservice_vehicle_stock/models/__init__.py
+++ fieldservice_vehicle_stock/models/__init__.py
@@ -1,7 +1,8 @@
-# Copyright (C) 2018 - TODAY, Brian McMaster
+# Copyright (C) 2018 Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
     fsm_vehicle,
     stock_request,
+    product_putaway,
 )
