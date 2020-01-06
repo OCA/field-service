8bea78c4bab5f4ed71612479f3245a0a388e50e4
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-01-06 17:37:35 -0600

    [FIX] fieldservice_route_vehicle

--- fieldservice_route_vehicle/models/fsm_route_dayroute.py
+++ fieldservice_route_vehicle/models/fsm_route_dayroute.py
@@ -7,10 +7,28 @@ from odoo import api, fields, models
 class FSMRouteDayRoute(models.Model):
     _inherit = 'fsm.route.dayroute'
 
-    fsm_vehicle_id = fields.Many2one('fsm.vehicle',
-                                     string='Vehicle')
+    @api.model
+    def _get_default_vehicle(self):
+        return self.route_id.fsm_vehicle_id.id or False
+
+    fsm_vehicle_id = fields.Many2one(
+        'fsm.vehicle', string='Vehicle', index=True,
+        default=_get_default_vehicle)
 
     @api.onchange('route_id')
     def _onchange_route_id(self):
         self.fsm_vehicle_id = self.route_id and \
             self.route_id.fsm_vehicle_id or False
+
+    @api.multi
+    def write(self, vals):
+        res = super().write(vals)
+        if vals.get('vehicle_id', False):
+            for order in self.order_ids:
+                for picking in order.picking_ids:
+                    if picking.state in ('waiting', 'confirmed'):
+                        picking.vehicle_id = vals.get('vehicle_id')
+                        # Set the vehicle on the previous picking as well
+                        if picking.move_line_ids[0].move_orig_ids[0]:
+                            picking.move_line_ids[0].move_orig_ids[0].picking_id.vehicle_id = vals.get('vehicle_id')
+        return res
