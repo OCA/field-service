ce379bb9f07b6a714956fd9c0a5785e9629d71ce
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-01-09 08:09:22 -0800

    [IMP] fieldservice_vehicle

--- fieldservice_vehicle/models/__init__.py
+++ fieldservice_vehicle/models/__init__.py
@@ -1,6 +1,7 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2018 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import (
     fsm_vehicle,
+    fsm_person,
+    fsm_order,
 )
