1cd1f02c9f0d6fa7bdbe1e5aa72cf9b460b3308a
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-01-16 17:49:49 -0600

    [FIX] fieldservice_account: Migration script

--- fieldservice_account/migrations/12.0.3.0.0/post-migration.py
+++ fieldservice_account/migrations/12.0.3.0.0/post-migration.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019, Open Source Integrators
+# Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
@@ -6,8 +6,9 @@ def migrate(env, version):
     if not version:
         return
 
-    env.execute("""INSERT INTO fsm_order_account_invoice_rel
-                SET (fsm_order_id, invoice_id) VALUES (
-                SELECT fsm_order_id, id
-                FROM account_invoice
-                WHERE fsm_order_id IS NOT NULL);""")
+    env.execute("""INSERT INTO
+    fsm_order_account_invoice_rel(fsm_order_id, invoice_id)
+    SELECT DISTINCT l.fsm_order_id, l.invoice_id
+    FROM account_invoice_line AS l
+    WHERE l.fsm_order_id IS NOT NULL AND l.invoice_id IS NOT NULL;
+    """)
