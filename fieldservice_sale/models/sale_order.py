8122d441f131131406e849d867daa881ba6d00e9
brian10048 <brian@mcmpest.com>
2020-01-16 17:40:44 -0500

    [FIX] fieldservice_sale

--- fieldservice_sale/models/sale_order.py
+++ fieldservice_sale/models/sale_order.py
@@ -106,7 +106,8 @@ class SaleOrder(models.Model):
         """
         # one search for all Sale Orders
         fsm_orders = self.env['fsm.order'].search([
-            ('sale_id', 'in', self.ids)])
+            ('sale_id', 'in', self.ids),
+            ('sale_line_id', '=', False)])
         fsm_order_mapping = {
             fsm_order.sale_id.id: fsm_order for fsm_order in fsm_orders}
         result = {}
