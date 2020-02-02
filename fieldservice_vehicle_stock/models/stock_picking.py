6c74b013ccf127e1cf425f3dd68182b26fb34890
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-02-01 19:15:43 -0600

    [FIX] fieldservice_vehicle_stock

--- fieldservice_vehicle_stock/models/stock_picking.py
+++ fieldservice_vehicle_stock/models/stock_picking.py
@@ -9,22 +9,25 @@ class StockPicking(models.Model):
 
     fsm_vehicle_id = fields.Many2one('fsm.vehicle', string='Vehicle')
 
+    @api.multi
     def action_assign(self):
-        if self.picking_type_id in (
-            # Vehicle Loading
-            self.env.ref(
-                'fieldservice_vehicle_stock.picking_type_output_to_vehicle'),
-            # Location Pickup
-            self.env.ref(
-                'fieldservice_vehicle_stock.picking_type_location_to_vehicle')
-        ):
-            if self.fsm_vehicle_id:
-                picking = self.with_context(vehicle_id=self.fsm_vehicle_id.id)
-                return super(StockPicking, picking).action_assign()
-            else:
-                raise UserError(_(
-                    "You must provide the vehicle for this picking type."))
-        return super().action_assign()
+        for rec in self:
+            if rec.picking_type_id in (
+                # Vehicle Loading
+                self.env.ref(
+                    'fieldservice_vehicle_stock.picking_type_output_to_vehicle'),
+                # Location Pickup
+                self.env.ref(
+                    'fieldservice_vehicle_stock.picking_type_location_to_vehicle')
+            ):
+                if rec.fsm_vehicle_id:
+                    picking = rec.with_context(vehicle_id=self.fsm_vehicle_id.id)
+                    return super(StockPicking, picking).action_assign()
+                else:
+                    raise UserError(_(
+                        "You must provide the vehicle for this picking type."))
+            res = super().action_assign()
+        return res
 
     @api.multi
     def write(self, vals):
