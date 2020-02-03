a27268e78358c867304a893f881636203a01fa34
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-02-03 11:15:26 -0600

    [IMP] fieldservice_vehicle_stock

--- fieldservice_vehicle_stock/models/stock_picking.py
+++ fieldservice_vehicle_stock/models/stock_picking.py
@@ -15,13 +15,16 @@ class StockPicking(models.Model):
             if rec.picking_type_id in (
                 # Vehicle Loading
                 self.env.ref(
-                    'fieldservice_vehicle_stock.picking_type_output_to_vehicle'),
+                    'fieldservice_vehicle_stock.'
+                    'picking_type_output_to_vehicle'),
                 # Location Pickup
                 self.env.ref(
-                    'fieldservice_vehicle_stock.picking_type_location_to_vehicle')
+                    'fieldservice_vehicle_stock.'
+                    'picking_type_location_to_vehicle')
             ):
                 if rec.fsm_vehicle_id:
-                    picking = rec.with_context(vehicle_id=self.fsm_vehicle_id.id)
+                    picking = \
+                        rec.with_context(vehicle_id=self.fsm_vehicle_id.id)
                     return super(StockPicking, picking).action_assign()
                 else:
                     raise UserError(_(
@@ -29,11 +32,17 @@ class StockPicking(models.Model):
             res = super().action_assign()
         return res
 
+    def prepare_fsm_values(self, fsm_order):
+        res = {}
+        if fsm_order:
+            res.update({
+                'fsm_vehicle_id': fsm_order.vehicle_id.id or False,
+            })
+        return res
+
     @api.multi
     def write(self, vals):
         if vals.get('fsm_order_id', False):
-            order = self.env['fsm.order'].browse(vals.get('fsm_order_id'))
-            vals.update({
-                'fsm_vehicle_id': order.vehicle_id.id or False,
-            })
+            fsm_order = self.env['fsm.order'].browse(vals.get('fsm_order_id'))
+            vals.update(self.prepare_fsm_values(fsm_order))
         return super().write(vals)
