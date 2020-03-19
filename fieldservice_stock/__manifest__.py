0f4173d5d6a270aa31f7e13fbb544c4ee9cfca72
Daniel Reis <dreis@opensourceintegrators.com>
2020-03-19 18:20:13 +0000

    [IMP] Add "Create FSM Equipment" option to Operation Types
    FSM Equipment creation for serial number tracked products can be
    on specific Operation Types, such as Delivery or Receipts.

--- fieldservice_stock/__manifest__.py
+++ fieldservice_stock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service - Stock',
     'summary': 'Integrate the logistics operations with Field Service',
-    'version': '12.0.1.4.0',
+    'version': '12.0.2.0.0',
     'category': 'Field Service',
     'author': "Open Source Integrators, "
               "Brian McMaster, "
@@ -24,6 +24,7 @@
         'views/stock.xml',
         'views/fsm_equipment.xml',
         'views/product_template.xml',
+        'views/stock_picking_type.xml',
         'views/stock_production_lot.xml',
         'views/stock_request.xml',
         'views/stock_request_order.xml',
