969ca1e6299342b0b201271db662356e8836f684
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-03-26 19:45:07 -0600

    [FIX] fieldservice_vehicle_stock

--- fieldservice_vehicle_stock/models/stock_picking.py
+++ fieldservice_vehicle_stock/models/stock_picking.py
@@ -25,11 +25,11 @@ class StockPicking(models.Model):
                 if rec.fsm_vehicle_id:
                     picking = \
                         rec.with_context(vehicle_id=rec.fsm_vehicle_id.id)
-                    return super(StockPicking, picking).action_assign()
+                    res = super(StockPicking, picking).action_assign()
                 else:
                     raise UserError(_(
                         "You must provide the vehicle for this picking type."))
-            res = super().action_assign()
+            res = super(StockPicking, rec).action_assign()
         return res
 
     def prepare_fsm_values(self, fsm_order):
