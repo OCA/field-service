0a425912a63453b7d8c4592e9db6b83e5840a5b2
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-05-05 14:57:39 -0500

    [FIX] fieldservice_vehicle_stock

--- fieldservice_vehicle_stock/models/stock_picking.py
+++ fieldservice_vehicle_stock/models/stock_picking.py
@@ -11,6 +11,7 @@ class StockPicking(models.Model):
 
     @api.multi
     def action_assign(self):
+        res = {}
         for rec in self:
             if rec.picking_type_id.require_vehicle_id:
                 if rec.fsm_vehicle_id:
