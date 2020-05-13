62af1bb8dc654f7aaac633ae780e7a9f90ae0f35
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2020-05-13 17:21:48 -0500

    [ADD] fieldservice_change_management
    Initial addition of Field Service - Change Management Module

---
+++ fieldservice_change_management/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2020 Pavlov Media <https://www.pavlovmedia.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service - Change Management',
+    'summary': 'Manage Change Logs on Locations',
+    'author': 'Patrick Wilson: Pavlov Media',
+    'website': 'https://www.pavlovmedia.com',
+    'category': 'Extra Tools',
+    'version': '12.0.1.0.0',
+    'license': 'Other proprietary',
+    'depends': ['mail',
+                'fieldservice',
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'data/ir_sequence.xml',
+        'data/change_log_impact.xml',
+        'data/change_log_stage.xml',
+        'data/change_log_type.xml',
+        'report/change_log_reports.xml',
+        'views/change_log.xml',
+        'views/change_log_impact.xml',
+        'views/change_log_stage.xml',
+        'views/change_log_tags.xml',
+        'views/change_log_type.xml',
+        'views/fsm_location.xml',
+    ],
+    'application': True,
+}
