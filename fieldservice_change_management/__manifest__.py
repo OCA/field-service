cec8df875a1c93a0074654f0039f69cdc791faaa
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2020-05-19 11:44:57 -0500

    Reviewer Suggested Changes
    Co-authored-by: Maxime Chambreuil <mchambreuil@opensourceintegrators.com>

--- fieldservice_change_management/__manifest__.py
+++ fieldservice_change_management/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Field Service - Change Management',
     'summary': 'Manage Change Logs on Locations',
-    'author': 'Patrick Wilson: Pavlov Media',
+    'author': 'Pavlov Media, Odoo Community Association (OCA)',
     'website': 'https://www.pavlovmedia.com',
     'category': 'Extra Tools',
     'version': '12.0.1.0.0',
