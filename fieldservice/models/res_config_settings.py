2335da2360cd7f42658e89ad193ac0ceb899e3bf
scampbell <scampbell@opensourceintegrators.com>
2020-05-19 19:48:40 -0700

    [IMP] User Options for Location Search

--- fieldservice/models/res_config_settings.py
+++ fieldservice/models/res_config_settings.py
@@ -81,6 +81,10 @@ class ResConfigSettings(models.TransientModel):
         string='Auto-populate equipments on Order based on the Location',
         related='company_id.auto_populate_equipments_on_order',
         readonly=False)
+    seach_on_complete_name = fields.Boolean(
+        string='Search Location By Hierarchy',
+        related='company_id.seach_on_complete_name',
+        readonly=False)
 
     # Dependencies
     @api.onchange('group_fsm_equipment')
