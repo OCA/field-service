1c9700544ee89d5dfe2a0e3edf6cd9ea3f10e62a
scampbell <scampbell@opensourceintegrators.com>
2020-05-28 12:54:08 -0700

    [IMP] Flake8

--- fieldservice/models/fsm_order.py
+++ fieldservice/models/fsm_order.py
@@ -54,9 +54,11 @@ class FSMOrder(models.Model):
     def _track_subtype(self, init_values):
         self.ensure_one()
         if 'stage_id' in init_values:
-            if self.stage_id.id == self.env.ref('fieldservice.fsm_stage_completed').id:
+            if self.stage_id.id == self.env.\
+                    ref('fieldservice.fsm_stage_completed').id:
                 return 'fieldservice.mt_order_completed'
-            if self.stage_id.id == self.env.ref('fieldservice.fsm_stage_cancelled').id:
+            if self.stage_id.id == self.env.\
+                    ref('fieldservice.fsm_stage_cancelled').id:
                 return 'fieldservice.mt_order_cancelled'
         return super()._track_subtype(init_values)
 
