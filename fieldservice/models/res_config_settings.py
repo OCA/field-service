721e6be36030f74d4d3bfcb21f5f165e19b686cf
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-06-03 08:24:31 -0500

    [FIX] fieldservice: Typo

--- fieldservice/models/res_config_settings.py
+++ fieldservice/models/res_config_settings.py
@@ -43,9 +43,11 @@ class ResConfigSettings(models.TransientModel):
     module_fieldservice_maintenance = fields.Boolean(
         string="Link FSM orders to maintenance requests"
     )
+    module_fieldservice_project = fields.Boolean(string="Projects and Tasks")
     module_fieldservice_purchase = fields.Boolean(
         string="Manage subcontractors and their pricelists"
     )
+    module_fieldservice_recurring = fields.Boolean(string="Manage Recurring Orders")
     module_fieldservice_repair = fields.Boolean(
         string="Link FSM orders to MRP Repair orders"
     )
@@ -64,8 +66,6 @@ class ResConfigSettings(models.TransientModel):
     module_fieldservice_stock = fields.Boolean(string="Use Odoo Logistics")
     module_fieldservice_vehicle = fields.Boolean(string="Manage Vehicles")
     module_fieldservice_substatus = fields.Boolean(string="Manage Sub-Statuses")
-    module_fieldservice_recurring = fields.Boolean(string="Manage Recurring Orders")
-    module_fieldservice_project = fields.Boolean(string="Projects and Tasks")
     module_fieldservice_web_timeline_view = fields.Boolean(
         string="Allow Field Service Web Timeline View"
     )
@@ -81,9 +81,9 @@ class ResConfigSettings(models.TransientModel):
         related="company_id.auto_populate_equipments_on_order",
         readonly=False,
     )
-    seach_on_complete_name = fields.Boolean(
+    search_on_complete_name = fields.Boolean(
         string="Search Location By Hierarchy",
-        related="company_id.seach_on_complete_name",
+        related="company_id.search_on_complete_name",
         readonly=False,
     )
 
