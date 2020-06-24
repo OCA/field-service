45c2f11a87a9096e11ec66120fac8b67389bd00b
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-06-23 21:41:06 -0300

    [12.0] fix type OS - fieldservice_maintenance

--- fieldservice_maintenance/models/fsm_order.py
+++ fieldservice_maintenance/models/fsm_order.py
@@ -15,9 +15,7 @@ class FSMOrder(models.Model):
         # if FSM order with type maintenance is create then
         # create maintenance request
         order = super(FSMOrder, self).create(vals)
-        maintenance = self.env.ref(
-            'fieldservice_maintenance.fsm_order_type_maintenance')
-        if order.type == maintenance:
+        if order.type.internal_type == 'maintenance':
             if order.equipment_id and not order.request_id:
                 equipment = order.equipment_id
                 maintenance_equipment_id = equipment.maintenance_equipment_id
