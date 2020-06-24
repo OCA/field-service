d918848f521a0c8a7b3b46b1d009f0164a7f648f
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-06-24 17:41:42 -0300

    [12.0] add internal type in fsm.order.type

--- fieldservice/models/fsm_order_type.py
+++ fieldservice/models/fsm_order_type.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import fields, models
 
 
@@ -8,3 +9,7 @@ class FSMOrderType(models.Model):
     _description = "Field Service Order Type"
 
     name = fields.Char(string="Name")
+
+    internal_type = fields.Selection(
+        string="Internal Type", selection=[("fsm", "FSM")], default="fsm",
+    )
