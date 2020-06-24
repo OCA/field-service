8db9ca73398cfce556faf3fa05b8fd376c53c2bc
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-06-24 20:13:58 -0300

    [12.0][FIX] fieldservice_repair

---
+++ fieldservice_repair/models/fsm_order_type.py
@@ -0,0 +1,14 @@
+# Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FsmOrderType(models.Model):
+
+    _inherit = 'fsm.order.type'
+
+    internal_type = fields.Selection(
+        selection_add=[('repair',
+                        'Repair')],
+    )
