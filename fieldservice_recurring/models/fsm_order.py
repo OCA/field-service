9403049323d48fc6e36b5eef5bac0c2c48a5a9f8
brian10048 <brian@mcmpest.com>
2020-07-17 21:58:01 -0400

    [MIG] fieldservice_recurring: Migration to 13.0

--- fieldservice_recurring/models/fsm_order.py
+++ fieldservice_recurring/models/fsm_order.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 - TODAY, Brian McMaster, Open Source Integrators
+# Copyright (C) 2019 Brian McMaster, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import timedelta
@@ -21,7 +21,6 @@ class FSMOrder(models.Model):
                 days_late = rec.fsm_recurring_id.fsm_frequency_set_id.buffer_late
                 rec.request_late = rec.scheduled_date_start + timedelta(days=days_late)
 
-    @api.multi
     def action_view_fsm_recurring(self):
         action = self.env.ref("fieldservice_recurring.action_fsm_recurring").read()[0]
         action["views"] = [
