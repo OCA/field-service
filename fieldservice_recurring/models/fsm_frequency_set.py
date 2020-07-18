ef866121a2b9c2275a00df311819436a245a01f3
brian10048 <brian@mcmpest.com>
2020-07-17 21:32:08 -0400

    [IMP] fieldservice_recurring: black, isort, prettier

--- fieldservice_recurring/models/fsm_frequency_set.py
+++ fieldservice_recurring/models/fsm_frequency_set.py
@@ -7,27 +7,34 @@ from odoo import fields, models
 
 
 class FSMFrequencySet(models.Model):
-    _name = 'fsm.frequency.set'
-    _description = 'Frequency Rule Set for Field Service Orders'
-    _inherit = ['mail.thread']
+    _name = "fsm.frequency.set"
+    _description = "Frequency Rule Set for Field Service Orders"
+    _inherit = ["mail.thread"]
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char("Name", required=True)
     active = fields.Boolean(default=True)
     fsm_frequency_ids = fields.Many2many(
-        'fsm.frequency', track_visibility='onchange',
-        string='Frequency Rules')
+        "fsm.frequency", track_visibility="onchange", string="Frequency Rules"
+    )
     schedule_days = fields.Integer(
-        string='Days Ahead to Schedule', default='30',
+        string="Days Ahead to Schedule",
+        default="30",
         help="""The number of days from today that the scheduler will generate
-            orders for this rule""", track_visibility='onchange')
+            orders for this rule""",
+        track_visibility="onchange",
+    )
     buffer_early = fields.Integer(
-        string='Early Buffer', track_visibility='onchange',
+        string="Early Buffer",
+        track_visibility="onchange",
         help="""The allowed number of days before the computed schedule date
-            that an event can be done""")
+            that an event can be done""",
+    )
     buffer_late = fields.Integer(
-        string='Late Buffer', track_visibility='onchange',
+        string="Late Buffer",
+        track_visibility="onchange",
         help="""The allowed number of days after the computed schedule date
-           that an event can be done""")
+           that an event can be done""",
+    )
 
     def _get_rruleset(self, dtstart=None, until=None):
         self.ensure_one()
