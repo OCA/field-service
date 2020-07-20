4faf125922c40e50b9e8ced66e49a56766b66e7e
brian10048 <brian@mcmpest.com>
2020-07-20 13:13:00 -0400

    [IMP] fieldservice_account: black, isort, prettier

--- fieldservice_account/migrations/12.0.3.0.0/post-migration.py
+++ fieldservice_account/migrations/12.0.3.0.0/post-migration.py
@@ -6,9 +6,11 @@ def migrate(env, version):
     if not version:
         return
 
-    env.execute("""INSERT INTO
-    fsm_order_account_invoice_rel(fsm_order_id, invoice_id)
-    SELECT DISTINCT l.fsm_order_id, l.invoice_id
-    FROM account_invoice_line AS l
-    WHERE l.fsm_order_id IS NOT NULL AND l.invoice_id IS NOT NULL;
-    """)
+    env.execute(
+        """INSERT INTO
+           fsm_order_account_invoice_rel(fsm_order_id, invoice_id)
+           SELECT DISTINCT l.fsm_order_id, l.invoice_id
+           FROM account_invoice_line AS l
+           WHERE l.fsm_order_id IS NOT NULL AND l.invoice_id IS NOT NULL;
+        """
+    )
