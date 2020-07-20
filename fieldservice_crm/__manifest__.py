853acef0db6209fbb05b606f7a9ee557777ecc46
brian10048 <brian@mcmpest.com>
2020-07-20 15:44:11 -0400

    [IMP] fieldservice_crm: black, isort, prettier

--- fieldservice_crm/__manifest__.py
+++ fieldservice_crm/__manifest__.py
@@ -2,26 +2,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - CRM',
-    'version': '12.0.1.1.0',
-    'summary': 'Create Field Service orders from the CRM',
-    'category': 'Field Service',
-    'author': 'Patrick Wilson, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/field-service',
-    'depends': [
-        'fieldservice',
-        'crm',
+    "name": "Field Service - CRM",
+    "version": "12.0.1.1.0",
+    "summary": "Create Field Service orders from the CRM",
+    "category": "Field Service",
+    "author": "Patrick Wilson, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "depends": ["fieldservice", "crm"],
+    "data": [
+        "views/crm_lead.xml",
+        "views/fsm_location.xml",
+        "views/fsm_order.xml",
+        "security/ir.model.access.csv",
     ],
-    'data': [
-        'views/crm_lead.xml',
-        'views/fsm_location.xml',
-        'views/fsm_order.xml',
-        'security/ir.model.access.csv',
-    ],
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': [
-        'patrickrwilson',
-    ],
-    'installable': True,
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["patrickrwilson"],
+    "installable": True,
 }
