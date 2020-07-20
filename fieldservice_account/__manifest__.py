db83a18bcbac835b338f7dd7836450df4b406c35
brian10048 <brian@mcmpest.com>
2020-07-20 13:13:00 -0400

    [IMP] fieldservice_account: black, isort, prettier

--- fieldservice_account/__manifest__.py
+++ fieldservice_account/__manifest__.py
@@ -2,26 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Accounting',
-    'summary': 'Track invoices linked to Field Service orders',
-    'version': '12.0.3.1.1',
-    'category': 'Field Service',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/field-service',
-    'depends': [
-        'fieldservice',
-        'account',
+    "name": "Field Service - Accounting",
+    "summary": "Track invoices linked to Field Service orders",
+    "version": "12.0.3.1.1",
+    "category": "Field Service",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "depends": ["fieldservice", "account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_invoice.xml",
+        "views/fsm_order.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/account_invoice.xml',
-        'views/fsm_order.xml',
-    ],
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': [
-        'osimallen',
-        'brian10048',
-        'bodedra',
-    ],
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["osimallen", "brian10048", "bodedra"],
 }
--- fieldservice_account/models/fsm_order.py
+++ fieldservice_account/models/fsm_order.py
@@ -4,28 +4,30 @@ from odoo import api, fields, models
 
 
 class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
+    _inherit = "fsm.order"
 
     invoice_ids = fields.Many2many(
-        'account.invoice', 'fsm_order_account_invoice_rel',
-        'fsm_order_id', 'invoice_id', string='Invoices/Bills')
+        "account.invoice",
+        "fsm_order_account_invoice_rel",
+        "fsm_order_id",
+        "invoice_id",
+        string="Invoices/Bills",
+    )
     invoice_count = fields.Integer(
-        string='Invoice Count',
-        compute='_compute_account_invoice_count', readonly=True)
+        string="Invoice Count", compute="_compute_account_invoice_count", readonly=True
+    )
 
-    @api.depends('invoice_ids')
+    @api.depends("invoice_ids")
     def _compute_account_invoice_count(self):
         for order in self:
             order.invoice_count = len(order.invoice_ids)
 
     @api.multi
     def action_view_invoices(self):
-        action = self.env.ref(
-            'account.action_invoice_tree').read()[0]
+        action = self.env.ref("account.action_invoice_tree").read()[0]
         if self.invoice_count > 1:
-            action['domain'] = [('id', 'in', self.invoice_ids.ids)]
+            action["domain"] = [("id", "in", self.invoice_ids.ids)]
         elif self.invoice_ids:
-            action['views'] = \
-                [(self.env.ref('account.invoice_form').id, 'form')]
-            action['res_id'] = self.invoice_ids[0].id
+            action["views"] = [(self.env.ref("account.invoice_form").id, "form")]
+            action["res_id"] = self.invoice_ids[0].id
         return action
