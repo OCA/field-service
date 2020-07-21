239eb4f251e1833a75e1c8789965254e03098b18
brian10048 <brian@mcmpest.com>
2020-07-21 00:32:06 -0400

    [IMP] fieldservice_stage_validation: black, isort, prettier

--- fieldservice_stage_validation/models/fsm_equipment.py
+++ fieldservice_stage_validation/models/fsm_equipment.py
@@ -5,9 +5,9 @@ from odoo.exceptions import ValidationError
 
 
 class FSMEquipment(models.Model):
-    _inherit = 'fsm.equipment'
+    _inherit = "fsm.equipment"
 
-    @api.constrains('stage_id')
+    @api.constrains("stage_id")
     def _validate_stage_fields(self):
         for rec in self:
             stage = rec.stage_id
@@ -17,6 +17,9 @@ class FSMEquipment(models.Model):
 
             for name in field_names:
                 if not values[0][name]:
-                    raise ValidationError(_('Cannot move to stage "%s" '
-                                            'until the "%s" field is set.'
-                                            % (stage.name, name)))
+                    raise ValidationError(
+                        _(
+                            'Cannot move to stage "%s" '
+                            'until the "%s" field is set.' % (stage.name, name)
+                        )
+                    )
--- fieldservice_stage_validation/models/fsm_stage.py
+++ fieldservice_stage_validation/models/fsm_stage.py
@@ -4,28 +4,27 @@ from odoo import api, fields, models
 
 
 class FSMStage(models.Model):
-    _inherit = 'fsm.stage'
+    _inherit = "fsm.stage"
 
-    @api.depends('stage_type')
+    @api.depends("stage_type")
     def _compute_stage_model(self):
         model_id = False
-        Model = self.env['ir.model']
+        Model = self.env["ir.model"]
         for rec in self:
             if rec.stage_type:
-                model_string = 'fsm.' + rec.stage_type
-                model_id = Model.search(
-                    [('model', '=', model_string)], limit=1).id
+                model_string = "fsm." + rec.stage_type
+                model_id = Model.search([("model", "=", model_string)], limit=1).id
             rec.stage_type_model_id = model_id
 
     validate_field_ids = fields.Many2many(
-        'ir.model.fields',
-        string='Fields to Validate',
-        help='Select fields which must be set on the document in this stage',
+        "ir.model.fields",
+        string="Fields to Validate",
+        help="Select fields which must be set on the document in this stage",
     )
 
     stage_type_model_id = fields.Many2one(
-        'ir.model',
+        "ir.model",
         compute=_compute_stage_model,
-        string='Model for Stage',
-        help='Technical field to hold model type',
+        string="Model for Stage",
+        help="Technical field to hold model type",
     )
