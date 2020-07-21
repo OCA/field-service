ec0f610cf3a025f1a531e985524fddb1ce44ce15
brian10048 <brian@mcmpest.com>
2020-07-21 00:11:41 -0400

    [IMP] fieldservice_skill: black, isort, prettier

--- fieldservice_skill/models/fsm_order.py
+++ fieldservice_skill/models/fsm_order.py
@@ -5,11 +5,11 @@ from odoo import api, fields, models
 
 
 class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
+    _inherit = "fsm.order"
 
-    skill_ids = fields.Many2many('hr.skill', string="Required Skills")
+    skill_ids = fields.Many2many("hr.skill", string="Required Skills")
 
-    @api.onchange('category_ids')
+    @api.onchange("category_ids")
     def _onchange_category_ids(self):
         if not self.template_id:
             skill_ids = []
@@ -17,7 +17,7 @@ class FSMOrder(models.Model):
                 skill_ids.extend([skill.id for skill in category.skill_ids])
             self.skill_ids = [(6, 0, skill_ids)]
 
-    @api.onchange('template_id')
+    @api.onchange("template_id")
     def _onchange_template_id(self):
         if self.template_id:
             super(FSMOrder, self)._onchange_template_id()
