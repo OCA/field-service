bf3386b5c33c589c22e1f2690a64d28fae17cd2f
brian10048 <brian@mcmpest.com>
2020-07-21 00:47:15 -0400

    [MIG] fieldservice_stage_server_action: Migration to 13.0

--- fieldservice_stage_server_action/__manifest__.py
+++ fieldservice_stage_server_action/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019, Open Source Integrators
 # # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "FSM Stage Server Action",
     "summary": "Execute server actions when reaching a Field Service stage",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/ursais/osi-addons",
