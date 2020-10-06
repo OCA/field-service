ee528538ed16d9e58f9453a667d5602f368cd9f2
Raphael <RLeeOSI@github.com>
2020-10-05 17:41:44 -0700

    [IMP] fieldservice_stock: black, isort, prettier

--- fieldservice_stock/models/fsm_location.py
+++ fieldservice_stock/models/fsm_location.py
@@ -5,16 +5,17 @@ from odoo import api, fields, models
 
 
 class FSMLocation(models.Model):
-    _inherit = 'fsm.location'
+    _inherit = "fsm.location"
 
     inventory_location_id = fields.Many2one(
-        'stock.location', string='Inventory Location', required=True,
-        default=lambda self: self.env.ref('stock.stock_location_customers'))
-    shipping_address_id = fields.Many2one('res.partner',
-                                          string='Shipping Location')
+        "stock.location",
+        string="Inventory Location",
+        required=True,
+        default=lambda self: self.env.ref("stock.stock_location_customers"),
+    )
+    shipping_address_id = fields.Many2one("res.partner", string="Shipping Location")
 
-    @api.onchange('fsm_parent_id')
+    @api.onchange("fsm_parent_id")
     def _onchange_fsm_parent_id(self):
         super(FSMLocation, self)._onchange_fsm_parent_id()
-        self.inventory_location_id = \
-            self.fsm_parent_id.inventory_location_id.id
+        self.inventory_location_id = self.fsm_parent_id.inventory_location_id.id
