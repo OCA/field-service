98872e707b438789a38e98ef6e756f458acdc395
Raphael <RLeeOSI@github.com>
2020-10-09 13:29:50 -0700

    [IMP] fieldservice_skill: black, isort, prettier

--- fieldservice_skill/models/__init__.py
+++ fieldservice_skill/models/__init__.py
@@ -1,10 +1,4 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import (
-    fsm_person_skill,
-    fsm_person,
-    fsm_category,
-    fsm_order,
-    fsm_template
-)
+from . import fsm_person_skill, fsm_person, fsm_category, fsm_order, fsm_template
