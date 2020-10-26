fb44d0819e30eacb48448e6b21827f15256ed353
Brian McMaster <brian@mcmpest.com>
2020-10-26 12:51:53 -0400

    [IMP] black, prettier

--- fieldservice/models/fsm_person_calendar_filter.py
+++ fieldservice/models/fsm_person_calendar_filter.py
@@ -6,16 +6,19 @@ from odoo import fields, models
 class FSMPersonCalendarFilter(models.Model):
     """ Assigned Worker Calendar Filter """
 
-    _name = 'fsm.person.calendar.filter'
-    _description = 'FSM Person Calendar Filter'
+    _name = "fsm.person.calendar.filter"
+    _description = "FSM Person Calendar Filter"
 
-    user_id = fields.Many2one('res.users', 'Me', required=True,
-                              default=lambda self: self.env.user)
-    fsm_person_id = fields.Many2one('fsm.person', 'FSM Worker', required=True)
-    active = fields.Boolean('Active', default=True)
+    user_id = fields.Many2one(
+        "res.users", "Me", required=True, default=lambda self: self.env.user
+    )
+    fsm_person_id = fields.Many2one("fsm.person", "FSM Worker", required=True)
+    active = fields.Boolean("Active", default=True)
 
     _sql_constraints = [
-        ('user_id_fsm_person_id_unique',
-         'UNIQUE(user_id,fsm_person_id)',
-         'You cannot have the same worker twice.')
+        (
+            "user_id_fsm_person_id_unique",
+            "UNIQUE(user_id,fsm_person_id)",
+            "You cannot have the same worker twice.",
+        )
     ]
