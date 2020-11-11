3d1c19d58cd8ca86edb649a37cbee6a2d95c07e7
hparfr <raph@hpar.fr>
2020-11-11 21:35:28 +0100

    fielservice_sale fix link with invoices

--- fieldservice_sale/models/sale_order_line.py
+++ fieldservice_sale/models/sale_order_line.py
@@ -145,5 +145,6 @@ class SaleOrderLine(models.Model):
 
     def _prepare_invoice_line(self):
         res = super()._prepare_invoice_line()
-        res.update({"fsm_order_id": self.fsm_order_id.id})
+        # by default: don't group fsm_orders on account.move.line
+        res.update({"fsm_order_ids": [(6, 0, [self.fsm_order_id.id])]})
         return res
