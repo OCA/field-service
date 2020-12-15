91af06b0dccc16ce5e7ac7cdc405c0e4d03aaf99
scampbell <scampbell@opensourceintegrators.com>
2020-12-14 20:19:35 -0700

    [IMP] Remove Test Case

--- fieldservice/tests/test_fsm_order.py
+++ fieldservice/tests/test_fsm_order.py
@@ -58,12 +58,3 @@ class TestFSMOrder(TransactionCase):
             order.scheduled_date_end,
             order.scheduled_date_start + timedelta(hours=duration),
         )
-        # Set new date end
-        order.scheduled_date_end = order.scheduled_date_end.replace(
-            hour=1, minute=1, second=0
-        )
-        # Check date start
-        self.assertEqual(
-            order.scheduled_date_start,
-            order.scheduled_date_end - timedelta(hours=duration),
-        )
