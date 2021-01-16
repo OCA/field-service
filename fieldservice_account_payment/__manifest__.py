5783aa181dc10f65f52049e5617da62a4b455803
brian10048 <brian@mcmpest.com>
2021-01-16 11:01:25 -0500

    [MIG] fieldservice_account_payment: Migration to 13.0

--- fieldservice_account_payment/__manifest__.py
+++ fieldservice_account_payment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Field Service - Accounting Payment",
     "summary": "Allow workers to collect payments from the order.",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
