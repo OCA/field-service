2aff0f7532759f609f50874f8587e2c6ed54c9e0
brian10048 <brian@mcmpest.com>
2021-01-16 10:42:06 -0500

    [IMP] fieldservice_account_payment: black, isort, prettier

--- fieldservice_account_payment/models/account_payment.py
+++ fieldservice_account_payment/models/account_payment.py
@@ -8,32 +8,36 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     fsm_order_ids = fields.Many2many(
-        'fsm.order', 'fsm_order_account_payment',
-        'payment_id', 'fsm_order_id', string='FSM Orders',
-        compute='_compute_fsm_order_ids',
-        store=True, index=True, copy=False)
+        "fsm.order",
+        "fsm_order_account_payment",
+        "payment_id",
+        "fsm_order_id",
+        string="FSM Orders",
+        compute="_compute_fsm_order_ids",
+        store=True,
+        index=True,
+        copy=False,
+    )
     fsm_order_count = fields.Integer(
-        string='FSM Order Count',
-        compute='_compute_fsm_order_count', readonly=True)
+        string="FSM Order Count", compute="_compute_fsm_order_count", readonly=True
+    )
 
-    @api.depends('fsm_order_ids')
+    @api.depends("fsm_order_ids")
     def _compute_fsm_order_count(self):
         for payment in self:
             payment.fsm_order_count = len(payment.fsm_order_ids)
 
     @api.multi
     def action_view_fsm_orders(self):
-        action = self.env.ref(
-            'fieldservice.action_fsm_operation_order').read()[0]
+        action = self.env.ref("fieldservice.action_fsm_operation_order").read()[0]
         if self.fsm_order_count > 1:
-            action['domain'] = [('id', 'in', self.fsm_order_ids)]
+            action["domain"] = [("id", "in", self.fsm_order_ids)]
         elif self.fsm_order_ids:
-            action['views'] = \
-                [(self.env.ref('fieldservice.fsm_order_form').id, 'form')]
-            action['res_id'] = self.fsm_order_ids[0].id
+            action["views"] = [(self.env.ref("fieldservice.fsm_order_form").id, "form")]
+            action["res_id"] = self.fsm_order_ids[0].id
         return action
 
-    @api.depends('invoice_ids.fsm_order_ids')
+    @api.depends("invoice_ids.fsm_order_ids")
     def _compute_fsm_order_ids(self):
         fsm_order_ids = []
         for invoice in self.invoice_ids:
