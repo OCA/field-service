141c1f280c3a0163c8619d6e8a2bd6b73919d04a
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-01-18 17:31:01 +0530

    [IMP] Code after pre-commit.

--- fieldservice_maintenance/models/fsm_order_type.py
+++ fieldservice_maintenance/models/fsm_order_type.py
@@ -6,9 +6,9 @@ from odoo import fields, models
 
 class FsmOrderType(models.Model):
 
-    _inherit = 'fsm.order.type'
+    _inherit = "fsm.order.type"
 
     internal_type = fields.Selection(
-        selection_add=[('maintenance',
-                        'Maintenance')],
+        selection_add=[("maintenance", "Maintenance")],
+        ondelete={"maintenance": "cascade"},
     )
