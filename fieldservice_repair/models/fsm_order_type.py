cd33f71c48ac306fbadb48e2b5782a796aa3c724
Freni-OSI <fpatel@opensourceintegrators.com>
2021-02-15 20:52:26 +0530

    [14.0][IMP]fieldservice_repair

--- fieldservice_repair/models/fsm_order_type.py
+++ fieldservice_repair/models/fsm_order_type.py
@@ -1,4 +1,4 @@
-# Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
+# Copyright 2021 - TODAY, Marcel Savegnago - Escodoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -6,9 +6,8 @@ from odoo import fields, models
 
 class FsmOrderType(models.Model):
 
-    _inherit = 'fsm.order.type'
+    _inherit = "fsm.order.type"
 
     internal_type = fields.Selection(
-        selection_add=[('repair',
-                        'Repair')],
+        selection_add=[("repair", "Repair")],
     )
