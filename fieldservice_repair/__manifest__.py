bc3ef8fa1ce33b3248e9852421275451f442a6ca
Freni-OSI <fpatel@opensourceintegrators.com>
2021-02-15 20:52:26 +0530

    [14.0][IMP]fieldservice_repair

--- fieldservice_repair/__manifest__.py
+++ fieldservice_repair/__manifest__.py
@@ -1,26 +1,23 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Repair',
-    'summary': 'Integrate Field Service orders with MRP repair orders',
-    'version': '12.0.1.1.0',
-    'category': 'Field Service',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/field-service',
-    'depends': [
-        'fieldservice_stock',
-        'repair',
+    "name": "Field Service - Repair",
+    "summary": "Integrate Field Service orders with MRP repair orders",
+    "version": "14.0.1.0.0",
+    "category": "Field Service",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "depends": [
+        "fieldservice_equipment_stock",
+        "repair",
     ],
-    'data': [
-        'data/fsm_order_type.xml',
-        'views/fsm_order_view.xml'
+    "data": ["data/fsm_order_type.xml", "views/fsm_order_view.xml"],
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": [
+        "smangukiya",
+        "max3903",
     ],
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': [
-        'smangukiya',
-        'max3903',
-    ],
-    'installable': True,
+    "installable": True,
 }
