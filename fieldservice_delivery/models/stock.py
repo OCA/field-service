a606a945699aa10d8a2bebd0f4ac7935696b5ee2
Freni-OSI <fpatel@opensourceintegrators.com>
2021-02-15 19:15:48 +0530

    [14.0][MIG]fieldservice_delivery

--- fieldservice_delivery/models/stock.py
+++ fieldservice_delivery/models/stock.py
@@ -1,19 +1,19 @@
-# Copyright (C) 2018 - TODAY, Brian McMaster
+# Copyright (C) 2021 - TODAY, Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     def _get_new_picking_values(self):
         vals = super(StockMove, self)._get_new_picking_values()
-        vals['carrier_id'] = self.fsm_order_id.carrier_id.id
+        vals["carrier_id"] = self.fsm_order_id.carrier_id.id
         return vals
 
 
 class ProcurementGroup(models.Model):
-    _inherit = 'procurement.group'
+    _inherit = "procurement.group"
 
-    carrier_id = fields.Many2one('delivery.carrier', string="Delivery Method")
+    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method")
