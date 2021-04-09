4648771af240afbd2fce1e6771fa777b1c007790
AmmrOSI <aofficewala@opensourceintegrators.com>
2021-04-09 22:11:43 +0530

    [14.0][MIG] Migrated a fieldservice_timeline module.

---
+++ fieldservice_timeline/models/fsm_person.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class FSMPerson(models.Model):
+    _name = "fsm.person"
+    _inherit = ["fsm.person", "mail.activity.mixin", "mail.thread.blacklist"]
