dc832551748a65677082a3bfe81f1a0e40e17138
Freni-OSI <fpatel@opensourceintegrators.com>
2021-04-14 16:25:02 +0530

    [14.0][MIG] fieldservice_account

--- fieldservice_account/__manifest__.py
+++ fieldservice_account/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Field Service - Accounting",
     "summary": "Track invoices linked to Field Service orders",
-    "version": "13.0.2.0.0",
+    "version": "14.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
