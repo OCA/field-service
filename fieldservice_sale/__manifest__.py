3232d2e987d0dc5c5473d9886cbefdd904c325da
Freni-OSI <fpatel@opensourceintegrators.com>
2021-04-15 14:48:53 +0530

    [14.0][MIG] fieldservice_sale

--- fieldservice_sale/__manifest__.py
+++ fieldservice_sale/__manifest__.py
@@ -7,18 +7,25 @@
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
-    "depends": ["fieldservice", "sale_management", "fieldservice_account"],
+    "depends": [
+        "fieldservice",
+        "sale_management",
+        "fieldservice_account",
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/fsm_location.xml",
         "views/fsm_order.xml",
         "views/product_template.xml",
-        "views/res_branch.xml",
         "views/sale_order.xml",
         "views/res_config_settings.xml",
     ],
     "license": "AGPL-3",
     "development_status": "Beta",
-    "maintainers": ["wolfhall", "max3903", "brian10048"],
+    "maintainers": [
+        "wolfhall",
+        "max3903",
+        "brian10048",
+    ],
     "installable": True,
 }
