2f7e7cb417aed08d6650a41dd41940fa3d4ae567
Freni-OSI <fpatel@opensourceintegrators.com>
2021-04-16 19:49:32 +0530

    [14.0][MIG] fieldservice_sale_recurring

--- fieldservice_sale_recurring/__manifest__.py
+++ fieldservice_sale_recurring/__manifest__.py
@@ -8,7 +8,10 @@
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
-    "depends": ["fieldservice_recurring", "fieldservice_sale"],
+    "depends": [
+        "fieldservice_recurring",
+        "fieldservice_sale",
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/fsm_recurring.xml",
@@ -17,7 +20,11 @@
     ],
     "license": "AGPL-3",
     "development_status": "Beta",
-    "maintainers": ["wolfhall", "max3903", "brian10048"],
+    "maintainers": [
+        "wolfhall",
+        "max3903",
+        "brian10048",
+    ],
     "installable": True,
     "auto_install": True,
 }
