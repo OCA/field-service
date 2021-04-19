7c7f875d7887dc5f59476f38b0351d60982a5bd4
Freni-OSI <fpatel@opensourceintegrators.com>
2021-04-19 13:38:26 +0530

    [14.0][MIG] fieldservice_vehicle

--- fieldservice_vehicle/__manifest__.py
+++ fieldservice_vehicle/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Field Service Vehicles",
     "summary": "Manage Field Service vehicles and assign drivers",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
