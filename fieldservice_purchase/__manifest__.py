b251b21a62584d6b6da2912e999fc2ad2c685811
Benoit Aimont <benoit.aimont@acsone.eu>
2021-04-22 10:41:59 +0200

    [IMP] fieldservice_purchase: black, isort, prettier

--- fieldservice_purchase/__manifest__.py
+++ fieldservice_purchase/__manifest__.py
@@ -2,24 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Purchase',
-    'summary': 'Manage FSM Purchases',
-    'author': 'Open Source Integrators, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/field-service',
-    'category': 'Field Service',
-    'license': 'AGPL-3',
-    'version': '12.0.1.1.0',
-    'depends': [
-        'fieldservice',
-        'purchase',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/fsm_person.xml',
-    ],
-    'development_status': 'Beta',
-    'maintainers': [
-        'osi-scampbell',
-    ],
+    "name": "Field Service - Purchase",
+    "summary": "Manage FSM Purchases",
+    "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "category": "Field Service",
+    "license": "AGPL-3",
+    "version": "12.0.1.1.0",
+    "depends": ["fieldservice", "purchase"],
+    "data": ["security/ir.model.access.csv", "views/fsm_person.xml"],
+    "development_status": "Beta",
+    "maintainers": ["osi-scampbell"],
 }
