3b2c47941f8c1bc3b07dbaf2c4d6782bbbe813de
Benoit Aimont <benoit.aimont@acsone.eu>
2021-04-28 15:29:42 +0200

    [IMP] fieldservice_account_analytic: black, isort, prettier

--- fieldservice_account_analytic/tests/test_fsm_account_wizard.py
+++ fieldservice_account_analytic/tests/test_fsm_account_wizard.py
@@ -1,13 +1,10 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo.addons.fieldservice_account.tests.test_fsm_account import (
-    FSMAccountCase
-)
+from odoo.addons.fieldservice_account.tests.test_fsm_account import FSMAccountCase
 
 
 class FSMAccountCase(FSMAccountCase):
-
     def setUp(self):
         super(FSMAccountCase, self).setUp()
 
@@ -20,11 +17,10 @@ class FSMAccountCase(FSMAccountCase):
         self.Wizard.action_convert_location(self.test_loc_partner2)
 
         # check if there is a new FSM Location with the same name
-        self.wiz_location = self.env['fsm.location']. \
-            search([('name', '=', self.test_loc_partner2.name)])
+        self.wiz_location = self.env["fsm.location"].search(
+            [("name", "=", self.test_loc_partner2.name)]
+        )
 
         # check if location is created successfully and fields copied over
-        self.assertEqual(self.test_loc_partner2,
-                         self.wiz_location.customer_id)
-        self.assertEqual(self.test_loc_partner2,
-                         self.wiz_location.owner_id)
+        self.assertEqual(self.test_loc_partner2, self.wiz_location.customer_id)
+        self.assertEqual(self.test_loc_partner2, self.wiz_location.owner_id)
