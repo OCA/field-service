00e86234195412a41000ffed28e775f5574f9803
Benoit Aimont <benoit.aimont@acsone.eu>
2021-04-28 09:57:44 +0200

    [MIG] fieldservice_isp_account: Migration to 13.0

--- fieldservice_isp_account/tests/test_isp_account.py
+++ fieldservice_isp_account/tests/test_isp_account.py
@@ -3,27 +3,42 @@
 
 from odoo import fields
 from odoo.exceptions import ValidationError
+from odoo.tests import SavepointCase
 
-from odoo.addons.fieldservice_account.tests.test_fsm_account import FSMAccountCase
 
-
-class FSMAccountCase(FSMAccountCase):
-    def setUp(self):
-        super(FSMAccountCase, self).setUp()
-        self.test_person = self.env["fsm.person"].create({"name": "Worker-1"})
-        self.test_analytic = self.env.ref("analytic.analytic_administratif")
-        self.account_id = self.env["account.account"].create(
+class FSMISPAccountCase(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(FSMISPAccountCase, cls).setUpClass()
+        cls.AccountMoveLine = cls.env["account.move.line"]
+        cls.test_person = cls.env["fsm.person"].create({"name": "Worker-1"})
+        cls.test_analytic = cls.env.ref("analytic.analytic_administratif")
+        cls.account_id = cls.env["account.account"].create(
             {
                 "code": "NC1110",
                 "name": "Test Payable Account",
-                "user_type_id": self.env.ref("account.data_account_type_payable").id,
+                "user_type_id": cls.env.ref("account.data_account_type_payable").id,
                 "reconcile": True,
+            }
+        )
+        # create a Res Partner to be converted to FSM Location/Person
+        cls.test_loc_partner = cls.env["res.partner"].create(
+            {"name": "Test Loc Partner", "phone": "ABC", "email": "tlp@email.com"}
+        )
+        # create expected FSM Location to compare to converted FSM Location
+        cls.test_location = cls.env["fsm.location"].create(
+            {
+                "name": "Test Location",
+                "phone": "123",
+                "email": "tp@email.com",
+                "partner_id": cls.test_loc_partner.id,
+                "owner_id": cls.test_loc_partner.id,
+                "customer_id": cls.test_loc_partner.id,
             }
         )
 
     def _create_workorder(self, bill_to, contractors, timesheets):
         # Create a new work order
-        contractors = self.env["account.invoice.line"].create(contractors)
         timesheets = self.env["account.analytic.line"].create(timesheets)
 
         order = self.env["fsm.order"].create(
@@ -31,10 +46,13 @@ class FSMAccountCase(FSMAccountCase):
                 "location_id": self.test_location.id,
                 "bill_to": bill_to,
                 "person_id": self.test_person.id,
-                "contractor_cost_ids": [(6, 0, contractors.ids)],
                 "employee_timesheet_ids": [(6, 0, timesheets.ids)],
             }
         )
+        for contractor in contractors:
+            contractor.update({"fsm_order_id": order.id})
+        contractors = self.env["fsm.order.cost"].create(contractors)
+        order.write({"contractor_cost_ids": [(6, 0, contractors.ids)]})
         order.person_ids += self.test_person
         return order
 
@@ -47,18 +65,18 @@ class FSMAccountCase(FSMAccountCase):
         self.assertEqual(order.account_stage, "review")
         # Create vendor bill
         # Vendor bill created from order's contractor
-        if not order.person_id.partner_id.supplier:
+        if not order.person_id.partner_id.supplier_rank:
             with self.assertRaises(ValidationError) as e:
                 order.account_confirm()
             self.assertEqual(
                 e.exception.name, "The worker assigned to this order is not a supplier"
             )
-            order.person_id.partner_id.supplier = True
+            order.person_id.partner_id.supplier_rank = True
         order.account_confirm()
         self.assertEqual(order.account_stage, "confirmed")
-        bill = self.AccountInvoice.search(
-            [("type", "=", "in_invoice"), ("fsm_order_id", "=", order.id)]
-        )
+        bill = self.AccountMoveLine.search(
+            [("fsm_order_ids", "in", order.id)]
+        ).move_id.filtered(lambda i: i.type == "in_invoice")
         self.assertEqual(len(bill), 1)
         self.assertEqual(len(order.contractor_cost_ids), len(bill.invoice_line_ids))
         # Customer invoice created from order's contractor and timehsheet
@@ -68,9 +86,9 @@ class FSMAccountCase(FSMAccountCase):
             order.customer_id = self.test_loc_partner  # Assign some partner
         order.account_create_invoice()
         self.assertEqual(order.account_stage, "invoiced")
-        invoice = self.AccountInvoice.search(
-            [("type", "=", "out_invoice"), ("fsm_order_id", "=", order.id)]
-        )
+        invoice = self.AccountMoveLine.search(
+            [("fsm_order_ids", "in", order.id)]
+        ).move_id.filtered(lambda i: i.type == "out_invoice")
         self.assertEqual(len(invoice), 1)
         self.assertEqual(
             len(order.contractor_cost_ids) + len(order.employee_timesheet_ids),
@@ -113,9 +131,7 @@ class FSMAccountCase(FSMAccountCase):
         self.test_location.analytic_account_id = self.test_analytic
         contractors = [
             {
-                "name": "contractor_line_1",
                 "product_id": self.env.ref("product.expense_hotel").id,
-                "account_id": self.account_id.id,
                 "quantity": 2,
                 "price_unit": 200,
             },
@@ -146,10 +162,9 @@ class FSMAccountCase(FSMAccountCase):
         self.assertEqual(order.contractor_total, 400)
         self.assertEqual(order.employee_time_total, 10)  # Hrs
         self.assertEqual(order.total_cost, 1400)
-        # Testing not working "Need to Configure Chart of Accounts"
-        # bill, invoice = self._process_order_to_invoices(order)
-        # self.assertEqual(bill.partner_id, order.person_id.partner_id)
-        # self.assertEqual(invoice.partner_id, order.location_id.customer_id)
+        bill, invoice = self._process_order_to_invoices(order)
+        self.assertEqual(bill.partner_id, order.person_id.partner_id)
+        self.assertEqual(invoice.partner_id, order.location_id.customer_id)
 
     def test_fsm_order_bill_to_contact(self):
         """Bill To Contact,
@@ -160,16 +175,12 @@ class FSMAccountCase(FSMAccountCase):
         # Create a new work order with contract = 500 and timesheet = 300
         contractors = [
             {
-                "name": "contractor_line_2",
                 "product_id": self.env.ref("product.expense_hotel").id,
-                "account_id": self.account_id.id,
                 "quantity": 2,
                 "price_unit": 100,
             },
             {
-                "name": "contractor_line_3",
                 "product_id": self.env.ref("product.expense_hotel").id,
-                "account_id": self.account_id.id,
                 "quantity": 1,
                 "price_unit": 300,
             },
