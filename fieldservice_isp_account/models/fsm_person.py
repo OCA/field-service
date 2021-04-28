c9cab3fbb80c9dcf0846e1afdc115f34b5fe5453
Benoit Aimont <benoit.aimont@acsone.eu>
2021-04-28 09:57:44 +0200

    [MIG] fieldservice_isp_account: Migration to 13.0

--- fieldservice_isp_account/models/fsm_person.py
+++ fieldservice_isp_account/models/fsm_person.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FSMPerson(models.Model):
@@ -10,19 +10,18 @@ class FSMPerson(models.Model):
     bill_count = fields.Integer(string="Vendor Bills", compute="_compute_vendor_bills")
 
     def _compute_vendor_bills(self):
-        self.bill_count = self.env["account.invoice"].search_count(
+        self.bill_count = self.env["account.move"].search_count(
             [("partner_id", "=", self.partner_id.id)]
         )
 
-    @api.multi
     def action_view_bills(self):
         for bill in self:
             action = self.env.ref("account.action_invoice_tree1").read()[0]
-            vendor_bills = self.env["account.invoice"].search(
+            vendor_bills = self.env["account.move"].search(
                 [("partner_id", "=", bill.partner_id.id)]
             )
             if len(vendor_bills) == 1:
-                action["views"] = [(self.env.ref("account.invoice_form").id, "form")]
+                action["views"] = [(self.env.ref("account.view_move_form").id, "form")]
                 action["res_id"] = vendor_bills.id
             else:
                 action["domain"] = [("id", "in", vendor_bills.ids)]
