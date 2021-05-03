5abc322385b8def33f6f714f21f993e4b563994f
Benoit Aimont <benoit.aimont@acsone.eu>
2021-05-03 09:19:34 +0200

    [IMP] fieldservice_stock_account_analytic: black, isort, prettier

--- fieldservice_stock_account_analytic/models/stock_request.py
+++ fieldservice_stock_account_analytic/models/stock_request.py
@@ -5,14 +5,13 @@ from odoo import api, models
 
 
 class StockRequest(models.Model):
-    _inherit = 'stock.request'
+    _inherit = "stock.request"
 
     @api.model
     def create(self, vals):
-        if 'fsm_order_id' in vals and vals.get('fsm_order_id'):
-            fsm_order = self.env['fsm.order'].browse(vals.get('fsm_order_id'))
-            vals.update({
-                'analytic_account_id':
-                    fsm_order.location_id.analytic_account_id.id
-            })
+        if "fsm_order_id" in vals and vals.get("fsm_order_id"):
+            fsm_order = self.env["fsm.order"].browse(vals.get("fsm_order_id"))
+            vals.update(
+                {"analytic_account_id": fsm_order.location_id.analytic_account_id.id}
+            )
         return super().create(vals)
