a47b15abfe2f1cfd1f49c9cdda8100552e580b1c
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2021-05-05 10:58:52 -0600

    [MIG] Black

--- fieldservice_agreement/tests/test_fsm_agreement.py
+++ fieldservice_agreement/tests/test_fsm_agreement.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields
-from odoo.tests.common import TransactionCase, Form
+from odoo.tests.common import Form, TransactionCase
 
 
 class FSMOrder(TransactionCase):
