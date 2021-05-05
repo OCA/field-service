899b701d4012cf3f1bdafabdd5b22327808dc9d2
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2021-05-05 10:49:10 -0600

    [14.0][MIG] fieldservice_agreement (Version 12.0 to 14.0)
    [MIG] Black
    
    [MIG] Added more coverage to tests
    
    [MIG] Add contract to dependancy
    
    Add maintainer
    
    Update CONTRIBUTORS.rst
    
    add eol

--- fieldservice_agreement/models/fsm_order.py
+++ fieldservice_agreement/models/fsm_order.py
@@ -5,8 +5,7 @@ from odoo import fields, models
 
 
 class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
+    _inherit = "fsm.order"
 
-    agreement_id = fields.Many2one('agreement', string='Agreement')
-    serviceprofile_id = fields.Many2one('agreement.serviceprofile',
-                                        'Service Profile')
+    agreement_id = fields.Many2one("agreement", string="Agreement")
+    serviceprofile_id = fields.Many2one("agreement.serviceprofile", "Service Profile")
