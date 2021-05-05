1d0e09ae93ed27c35347a0377c8a760fce0583e2
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2021-05-05 10:49:10 -0600

    [14.0][MIG] fieldservice_agreement (Version 12.0 to 14.0)
    [MIG] Black
    
    [MIG] Added more coverage to tests
    
    [MIG] Add contract to dependancy
    
    Add maintainer
    
    Update CONTRIBUTORS.rst
    
    add eol

--- fieldservice_agreement/__manifest__.py
+++ fieldservice_agreement/__manifest__.py
@@ -2,31 +2,28 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Agreements',
-    'summary': 'Manage Field Service agreements and contracts',
-    'author': 'Open Source Integrators, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/field-service',
-    'category': 'Field Service',
-    'license': 'AGPL-3',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'fieldservice',
-        'agreement_serviceprofile',
+    "name": "Field Service - Agreements",
+    "summary": "Manage Field Service agreements and contracts",
+    "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "category": "Field Service",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "depends": ["fieldservice", "agreement_serviceprofile"],
+    "data": [
+        "views/fsm_order_view.xml",
+        "views/fsm_equipment_view.xml",
+        "views/agreement_view.xml",
+        "views/fsm_person.xml",
+        "views/fsm_location.xml",
     ],
-    'data': [
-        'views/fsm_order_view.xml',
-        'views/fsm_equipment_view.xml',
-        'views/agreement_view.xml',
-        'views/fsm_person.xml',
-        'views/fsm_location.xml'
-    ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': [
-        'max3903',
-        'bodedra',
-        'smangukiya',
-        'osi-scampbell'
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": [
+        "max3903",
+        "bodedra",
+        "smangukiya",
+        "osi-scampbell",
+        "patrickrwilson",
     ],
 }
--- fieldservice_agreement/models/fsm_location.py
+++ fieldservice_agreement/models/fsm_location.py
@@ -1,25 +1,28 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FSMLocation(models.Model):
-    _inherit = 'fsm.location'
+    _inherit = "fsm.location"
 
-    serviceprofile_ids = fields.Many2many('agreement.serviceprofile',
-                                          string="Service Profiles",
-                                          compute='_compute_service_ids')
+    serviceprofile_ids = fields.Many2many(
+        "agreement.serviceprofile",
+        string="Service Profiles",
+        compute="_compute_service_ids",
+    )
 
-    @api.multi
     def _compute_service_ids(self):
         for loc in self:
-            agreements = self.env['agreement'].\
-                search([('fsm_location_id', '=', loc.id)])
+            agreements = self.env["agreement"].search(
+                [("fsm_location_id", "=", loc.id)]
+            )
             ids = []
             for agree in agreements:
-                servpros = self.env['agreement.serviceprofile'].\
-                    search([('agreement_id', '=', agree.id)])
+                servpros = self.env["agreement.serviceprofile"].search(
+                    [("agreement_id", "=", agree.id)]
+                )
                 for ser in servpros:
                     if ser.id not in ids:
                         ids.append(ser.id)
