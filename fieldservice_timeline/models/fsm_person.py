502cb7d3cc704750febf47b58bd02e0eaa724e7b
Freni-OSI <fpatel@opensourceintegrators.com>
2021-05-20 16:11:24 +0530

    [IMP] Improved Code

--- fieldservice_timeline/models/fsm_person.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2021 Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import models
-
-
-class FSMPerson(models.Model):
-    _name = "fsm.person"
-    _inherit = ["fsm.person", "mail.thread.blacklist"]
