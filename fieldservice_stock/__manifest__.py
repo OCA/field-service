7de0d86a9a2f911e9e50541601347455bebee4b6
Brian McMaster <brian@mcmpest.com>
2021-05-26 11:23:36 -0400

    [IMP] fieldservice_stock: black, isort, prettier

--- fieldservice_stock/__manifest__.py
+++ fieldservice_stock/__manifest__.py
@@ -2,32 +2,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Stock',
-    'summary': 'Integrate the logistics operations with Field Service',
-    'version': '12.0.3.0.0',
-    'category': 'Field Service',
-    'author': "Open Source Integrators, "
-              "Brian McMaster, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/field-service',
-    'depends': [
-        'fieldservice',
-        'stock',
+    "name": "Field Service - Stock",
+    "summary": "Integrate the logistics operations with Field Service",
+    "version": "12.0.3.0.0",
+    "category": "Field Service",
+    "author": "Open Source Integrators, "
+    "Brian McMaster, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "depends": ["fieldservice", "stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/fsm_stock_data.xml",
+        "views/fsm_territory.xml",
+        "views/fsm_location.xml",
+        "views/fsm_order.xml",
+        "views/stock.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/fsm_stock_data.xml',
-        'views/fsm_territory.xml',
-        'views/fsm_location.xml',
-        'views/fsm_order.xml',
-        'views/stock.xml',
-    ],
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': [
-        'brian10048',
-        'wolfhall',
-        'max3903',
-        'smangukiya',
-    ],
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["brian10048", "wolfhall", "max3903", "smangukiya"],
 }
