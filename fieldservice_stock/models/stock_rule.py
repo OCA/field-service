1da238400c9e0ba52565bd06edba114e36f1ffd6
Brian McMaster <brian@mcmpest.com>
2021-05-26 11:23:36 -0400

    [IMP] fieldservice_stock: black, isort, prettier

--- fieldservice_stock/models/stock_rule.py
+++ fieldservice_stock/models/stock_rule.py
@@ -4,12 +4,28 @@ from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
+    def _get_stock_move_values(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        group_id,
+    ):
         vals = super()._get_stock_move_values(
-            product_id, product_qty, product_uom, location_id, name, origin,
-            values, group_id)
-        vals.update({'fsm_order_id': values.get('fsm_order_id')})
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            values,
+            group_id,
+        )
+        vals.update({"fsm_order_id": values.get("fsm_order_id")})
         return vals
