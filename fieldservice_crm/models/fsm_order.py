2e4d3211069007ac5516ff9f2e80ad87ac8b1ca8
Chanakya Soni <csoni@opensourceintegrators.com>
2021-06-04 11:21:39 +0530

    [14.0][MIG] fieldservice_crm.

--- fieldservice_crm/models/fsm_order.py
+++ fieldservice_crm/models/fsm_order.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class FSMOrder(models.Model):
     _inherit = "fsm.order"
 
-    opportunity_id = fields.Many2one(
-        "crm.lead", string="Opportunity", track_visibility="onchange"
-    )
+    opportunity_id = fields.Many2one("crm.lead", string="Opportunity", tracking=True)
