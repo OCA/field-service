7471bc881fde11dec517f3907c664514ac9d6a47
Freni-OSI <fpatel@opensourceintegrators.com>
2021-06-09 16:46:22 +0530

    [14.0][FIX] Travis

--- fieldservice_isp_account/__manifest__.py
+++ fieldservice_isp_account/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Field Service - ISP Accounting",
     "summary": """Invoice Field Service orders based on employee time
                   or contractor costs""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
