11e1810e5b4fc942b893cf3833078289a0366da2
Benoit Aimont <benoit.aimont@acsone.eu>
2021-06-10 10:45:29 +0200

    [IMP] make fsm order vals when creating fsm order from maintenance request creation inheritable

--- fieldservice_maintenance/models/maintenance_request.py
+++ fieldservice_maintenance/models/maintenance_request.py
@@ -17,26 +17,27 @@ class MaintenanceRequest(models.Model):
         request = super(MaintenanceRequest, self).create(vals)
         ctx = dict(self._context)
         if request.equipment_id.is_fsm_equipment and "fsm_order" not in ctx:
-            # Get the fsm equipment
-            fsm_equipment = self.env["fsm.equipment"].search(
-                [("maintenance_equipment_id", "=", request.equipment_id.id)], limit=1
-            )
-            fsm_order_type = self.env["fsm.order.type"].search(
-                [("internal_type", "=", "maintenance")], order="id desc", limit=1
-            )
-            if not fsm_equipment.current_location_id.id:
-                raise UserError(
-                    _("Missing current location on FSM equipment %s")
-                    % fsm_equipment.name
-                )
-            fsm_order_id = self.env["fsm.order"].create(
-                {
-                    "type": fsm_order_type.id,
-                    "equipment_id": fsm_equipment.id,
-                    "location_id": fsm_equipment.current_location_id.id,
-                    "request_id": request.id,
-                    "description": request.description,
-                }
-            )
+            fsm_order_vals = self._get_fsm_order_vals(request)
+            fsm_order_id = self.env["fsm.order"].create(fsm_order_vals)
             request.fsm_order_id = fsm_order_id
         return request
+
+    def _get_fsm_order_vals(self, request):
+        # Get the fsm equipment
+        fsm_equipment = self.env["fsm.equipment"].search(
+            [("maintenance_equipment_id", "=", request.equipment_id.id)], limit=1
+        )
+        fsm_order_type = self.env["fsm.order.type"].search(
+            [("internal_type", "=", "maintenance")], order="id desc", limit=1
+        )
+        if not fsm_equipment.current_location_id.id:
+            raise UserError(
+                _("Missing current location on FSM equipment %s") % fsm_equipment.name
+            )
+        return {
+            "type": fsm_order_type.id,
+            "equipment_id": fsm_equipment.id,
+            "location_id": fsm_equipment.current_location_id.id,
+            "request_id": request.id,
+            "description": request.description,
+        }
