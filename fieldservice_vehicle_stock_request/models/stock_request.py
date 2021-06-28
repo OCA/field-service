f451dbf492d4b6774d97988e51d68107923f8e58
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-06-28 18:34:56 +0530

    [14.0][MIG]fieldservice_vehicle_stock_request: migrated to v14

--- fieldservice_vehicle_stock_request/models/stock_request.py
+++ fieldservice_vehicle_stock_request/models/stock_request.py
@@ -1,33 +1,31 @@
-# Copyright (C) 2018 - TODAY, Brian McMaster
+# Copyright (C) 2021 - TODAY, Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class StockRequest(models.Model):
-    _inherit = 'stock.request'
+    _inherit = "stock.request"
 
-    @api.multi
     def action_assign(self):
         for rec in self:
             for move in rec.move_ids:
                 if move.picking_id.picking_type_id == self.env.ref(
-                        'fieldservice_vehicle_stock.'
-                        'picking_type_vehicle_to_location'):
+                    "fieldservice_vehicle_stock.picking_type_vehicle_to_location"
+                ):
                     return move.picking_id.action_assign()
 
-    @api.multi
     def action_deliver(self):
         for rec in self:
             for move in rec.move_ids:
                 if move.picking_id.picking_type_id == self.env.ref(
-                        'fieldservice_vehicle_stock.'
-                        'picking_type_vehicle_to_location'):
-                    return move.picking_id.action_done()
+                    "fieldservice_vehicle_stock.picking_type_vehicle_to_location"
+                ):
+                    return move.picking_id.button_validate()
 
     def action_show_details(self):
         for move in self.move_ids:
             if move.picking_id.picking_type_id == self.env.ref(
-                    'fieldservice_vehicle_stock.'
-                    'picking_type_vehicle_to_location'):
+                "fieldservice_vehicle_stock.picking_type_vehicle_to_location"
+            ):
                 return move.action_show_details()
