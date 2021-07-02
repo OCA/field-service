d52fbc62feb1553f47236b930a1995b9582f872e
Reed Hayashikawa <rhayashikawa@opensourceintegrators.com>
2021-07-02 09:23:57 -0700

    [IMP] fieldservice_equipment_stock: black, isort, prettier

--- fieldservice_equipment_stock/models/product_template.py
+++ fieldservice_equipment_stock/models/product_template.py
@@ -5,6 +5,6 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    create_fsm_equipment = fields.Boolean(string='Creates a FSM Equipment')
+    create_fsm_equipment = fields.Boolean(string="Creates a FSM Equipment")
