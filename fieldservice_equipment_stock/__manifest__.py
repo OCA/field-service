1465adf712aca03529b08c6119a1bddcf6b6a25b
Reed Hayashikawa <rhayashikawa@opensourceintegrators.com>
2021-07-02 10:17:03 -0700

    [MIG] fieldservice_equipment_stock: Migration to 14.0

--- fieldservice_equipment_stock/__manifest__.py
+++ fieldservice_equipment_stock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Field Service - Stock Equipment",
     "summary": "Integrate stock operations with your field service equipments",
-    "version": "12.0.2.0.0",
+    "version": "14.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, "
     "Brian McMaster, "
