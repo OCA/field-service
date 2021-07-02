21148795545d5eadb0bffd2cb736623f20857ce1
Reed Hayashikawa <rhayashikawa@opensourceintegrators.com>
2021-07-02 09:23:57 -0700

    [IMP] fieldservice_equipment_stock: black, isort, prettier

--- fieldservice_equipment_stock/models/stock_move.py
+++ fieldservice_equipment_stock/models/stock_move.py
@@ -7,24 +7,24 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def prepare_equipment_values(self, move_line):
-        return {'name': '%s (%s)' % (
-            move_line.product_id.name, move_line.lot_id.name),
-            'product_id': move_line.product_id.id,
-            'lot_id': move_line.lot_id.id,
-            'location_id':
-            move_line.move_id.stock_request_ids.fsm_order_id.location_id.id,
-            'current_location_id':
-            move_line.move_id.stock_request_ids.fsm_order_id.location_id.id,
-            'current_stock_location_id': move_line.location_dest_id.id}
+        return {
+            "name": "%s (%s)" % (move_line.product_id.name, move_line.lot_id.name),
+            "product_id": move_line.product_id.id,
+            "lot_id": move_line.lot_id.id,
+            "location_id": move_line.move_id.stock_request_ids.fsm_order_id.location_id.id,
+            "current_location_id": move_line.move_id.stock_request_ids.fsm_order_id.location_id.id,
+            "current_stock_location_id": move_line.location_dest_id.id,
+        }
 
     def _action_done(self):
         res = super()._action_done()
         for rec in self:
-            if (rec.state == 'done'
-                    and rec.picking_type_id.create_fsm_equipment
-                    and rec.product_tmpl_id.create_fsm_equipment):
+            if (
+                rec.state == "done"
+                and rec.picking_type_id.create_fsm_equipment
+                and rec.product_tmpl_id.create_fsm_equipment
+            ):
                 for line in rec.move_line_ids:
                     vals = self.prepare_equipment_values(line)
-                    line.lot_id.fsm_equipment_id = \
-                        rec.env['fsm.equipment'].create(vals)
+                    line.lot_id.fsm_equipment_id = rec.env["fsm.equipment"].create(vals)
         return res
--- fieldservice_equipment_stock/tests/test_fsm_equipment.py
+++ fieldservice_equipment_stock/tests/test_fsm_equipment.py
@@ -4,34 +4,41 @@ from odoo.tests.common import TransactionCase
 
 
 class TestFSMEquipment(TransactionCase):
-
     def setUp(self):
         super(TestFSMEquipment, self).setUp()
-        self.Equipment = self.env['fsm.equipment']
-        self.stock_location = self.env.ref('stock.stock_location_customers')
+        self.Equipment = self.env["fsm.equipment"]
+        self.stock_location = self.env.ref("stock.stock_location_customers")
 
-        product1 = self.env['product.product'].create({
-            'name': 'Product A',
-            'type': 'product',
-            'tracking': 'serial',
-        })
-        lot1 = self.env['stock.production.lot'].create({
-            'name': 'serial1',
-            'product_id': product1.id,
-        })
-        self.env['stock.quant'].create({
-            'product_id': product1.id,
-            'location_id': self.stock_location.id,
-            'quantity': 1.0,
-            'lot_id': lot1.id,
-        })
+        product1 = self.env["product.product"].create(
+            {
+                "name": "Product A",
+                "type": "product",
+                "tracking": "serial",
+            }
+        )
+        lot1 = self.env["stock.production.lot"].create(
+            {
+                "name": "serial1",
+                "product_id": product1.id,
+            }
+        )
+        self.env["stock.quant"].create(
+            {
+                "product_id": product1.id,
+                "location_id": self.stock_location.id,
+                "quantity": 1.0,
+                "lot_id": lot1.id,
+            }
+        )
 
-        self.equipment = self.Equipment.create({
-            'name': 'Equipment 1',
-            'product_id': product1.id,
-            'lot_id': lot1.id,
-            'current_stock_location_id': self.stock_location.id,
-        })
+        self.equipment = self.Equipment.create(
+            {
+                "name": "Equipment 1",
+                "product_id": product1.id,
+                "lot_id": lot1.id,
+                "current_stock_location_id": self.stock_location.id,
+            }
+        )
 
     def test_onchange_product(self):
         equipment = self.equipment
