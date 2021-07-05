7d62cdf24d7c866704db13108e74ddd94c6189ad
Freni-OSI <fpatel@opensourceintegrators.com>
2021-07-05 14:24:54 +0530

    [codecov]

--- fieldservice_stock/tests/common.py
+++ fieldservice_stock/tests/common.py
@@ -1,7 +1,10 @@
 # Copyright (C) 2020, Brian McMaster
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo.tests.common import SavepointCase
+from datetime import timedelta
+
+from odoo import fields
+from odoo.tests.common import Form, SavepointCase
 
 
 class TestFSMStockCommon(SavepointCase):
@@ -29,3 +32,17 @@ class TestFSMStockCommon(SavepointCase):
                 "inventory_location_id": cls.stock_cust_loc,
             }
         )
+        cls.fsm_location_1._onchange_fsm_parent_id()
+
+    def test_fsm_orders(self):
+        """Test creating new workorders, and test following functions."""
+        # Create an Orders
+        view_id = "fieldservice.fsm_order_form"
+        hours_diff = 100
+        with Form(self.Order, view=view_id) as f:
+            f.location_id = self.test_location
+            f.date_start = fields.Datetime.today()
+            f.date_end = f.date_start + timedelta(hours=hours_diff)
+            f.request_early = fields.Datetime.today()
+        order = f.save()
+        order._default_warehouse_id()
