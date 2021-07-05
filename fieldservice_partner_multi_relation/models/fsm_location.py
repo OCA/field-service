71a48739560279882141b99d0c6f0c43966f4580
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-05 14:38:38 +0530

    [14.0][MIG]fieldservice_partner_multi_relation: migrated to v14

--- fieldservice_partner_multi_relation/models/fsm_location.py
+++ fieldservice_partner_multi_relation/models/fsm_location.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FSMLocation(models.Model):
@@ -9,23 +9,18 @@ class FSMLocation(models.Model):
 
     rel_count = fields.Integer(string="Relations", compute="_compute_relation_count")
 
-    @api.multi
     def _compute_relation_count(self):
         for location in self:
             location.rel_count = self.env["res.partner.relation.all"].search_count(
                 [("this_partner_id", "=", location.name)]
             )
 
-    @api.multi
     def action_view_relations(self):
         """
         This function returns an action that display existing
         partner relations of a given fsm location id.
         """
         for location in self:
-            action = self.env.ref(
-                "partner_multi_relation.tree_res_partner_relation_all"
-            ).read()[0]
             relations = self.env["res.partner.relation.all"].search(
                 [("this_partner_id", "=", location.name)]
             )
---
+++ fieldservice_partner_multi_relation/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2021 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_fieldservice_partner_multi_relation
