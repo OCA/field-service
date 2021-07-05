9aec578f8162554fe100943dd7e17796fca9e427
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-05 12:13:49 +0530

    [14.0][MIG]fieldservice_partner_multi_relation: Travis

--- fieldservice_partner_multi_relation/__manifest__.py
+++ fieldservice_partner_multi_relation/__manifest__.py
@@ -3,20 +3,14 @@
 {
     "name": "Field Service Partner Relations",
     "summary": "Manage relations between contacts, companies and locations",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "complexity": "normal",
     "license": "AGPL-3",
-    "depends": [
-        "partner_multi_relation",
-        "fieldservice"
-    ],
-    "data": [
-        "views/fsm_location.xml",
-        "views/menu.xml"
-    ],
+    "depends": ["partner_multi_relation", "fieldservice"],
+    "data": ["views/fsm_location.xml", "views/menu.xml"],
     "demo": [
         "data/demo.xml",
     ],
