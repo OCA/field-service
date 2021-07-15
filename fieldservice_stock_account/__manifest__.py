9bdf95605f776ea5062f4a9c2683ef46eedb2344
Freni-OSI <fpatel@opensourceintegrators.com>
2021-07-15 17:01:10 +0530

    [14.0][FIX] Travis

--- fieldservice_stock_account/__manifest__.py
+++ fieldservice_stock_account/__manifest__.py
@@ -2,24 +2,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Field Service - Stock Account',
-    'version': '12.0.2.0.0',
-    'category': 'Field Service',
-    'summary': 'Invoice inventory items delivered with Field Service orders',
-    'author': 'Open Source Integrators, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/field-service',
-    'depends': [
-        'fieldservice_account',
-        'fieldservice_stock_request',
-        'account_invoice_report_hide_line',
+    "name": "Field Service - Stock Account",
+    "version": "14.0.1.0.0",
+    "category": "Field Service",
+    "summary": "Invoice inventory items delivered with Field Service orders",
+    "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "depends": [
+        "fieldservice_account",
+        "fieldservice_stock_request",
+        "account_invoice_report_hide_line",
     ],
-    'data': [],
-    'installable': True,
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': [
-        'bodedra',
-        'max3903',
+    "data": [],
+    "installable": True,
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": [
+        "bodedra",
+        "max3903",
     ],
 }
