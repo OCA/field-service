76b6ba453cf7c16ea566ef1b656c0c6d96d7de16
Chanakya Soni <chanakya.soni@serpentcs.com>
2021-09-15 10:48:23 +0530

    [FIX] Fixed OU script issue

--- fieldservice_account/migrations/13.0.2.0.0/pre-migration.py
+++ fieldservice_account/migrations/13.0.2.0.0/pre-migration.py
@@ -1,13 +1,30 @@
+# Copyright (C) 2018, Open Source Integrators
 # Copyright 2020 Akretion <raphael.reverdy@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openupgradelib import openupgrade
 
-column_renames = {
-    "account_move_line": [("fsm_order_id", None)],
-}
+
+def update_fsm_order_in_aml(cr):
+    """ Update fsm_order_id in account.move.line to use in post migration """
+    openupgrade.logged_query(
+        cr,
+        """
+            ALTER TABLE account_move_line ADD COLUMN fsm_order_id INTEGER
+        """,
+    )
+    openupgrade.logged_query(
+        cr,
+        """update account_move_line
+        set
+            fsm_order_id=ail.fsm_order_id
+        from
+            account_invoice_line ail
+        where
+            ail.id=account_move_line.old_invoice_line_id""",
+    )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_columns(env.cr, column_renames)
+    update_fsm_order_in_aml(env.cr)
