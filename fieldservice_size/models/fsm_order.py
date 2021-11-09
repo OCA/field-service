1bd1f4c6ad2c6d0bdef6b7ab1ac08b0e8d8236f8
Brian McMaster <brian@mcmpest.com>
2021-11-09 10:02:51 -0500

    [IMP] fieldservice_size: black, isort, prettier

--- fieldservice_size/models/fsm_order.py
+++ fieldservice_size/models/fsm_order.py
@@ -4,24 +4,28 @@ from odoo import api, fields, models
 
 
 class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
+    _inherit = "fsm.order"
 
     def _default_size_id(self):
         type_id = self.type.id
         size = False
         if type_id:
-            size = self.env['fsm.size'].search(
-                [('type_id', '=', type_id),
-                 ('is_order_size', '=', True)], limit=1)
+            size = self.env["fsm.size"].search(
+                [("type_id", "=", type_id), ("is_order_size", "=", True)], limit=1
+            )
         return size
 
     def _default_size_value(self):
         size_id = self.size_id
         size_value = 0
         if size_id:
-            size = self.env['fsm.location.size'].search(
-                [('location_id', '=', self.location_id.id),
-                 ('size_id', '=', size_id.id)], limit=1)
+            size = self.env["fsm.location.size"].search(
+                [
+                    ("location_id", "=", self.location_id.id),
+                    ("size_id", "=", size_id.id),
+                ],
+                limit=1,
+            )
             if size:
                 size_value = size.quantity
         return size_value
@@ -33,25 +37,26 @@ class FSMOrder(models.Model):
             uom = size.uom_id
         return uom
 
-    size_id = fields.Many2one('fsm.size', default=_default_size_id)
-    size_value = fields.Float(string='Order Size', default=_default_size_value)
-    size_uom = fields.Many2one('uom.uom', string='Unit of Measure',
-                               default=_default_size_uom)
+    size_id = fields.Many2one("fsm.size", default=_default_size_id)
+    size_value = fields.Float(string="Order Size", default=_default_size_value)
+    size_uom = fields.Many2one(
+        "uom.uom", string="Unit of Measure", default=_default_size_uom
+    )
 
-    @api.onchange('location_id')
+    @api.onchange("location_id")
     def onchange_location_id(self):
         super().onchange_location_id()
         self.size_id = self._default_size_id()
         self.size_value = self._default_size_value()
         self.size_uom = self._default_size_uom()
 
-    @api.onchange('type')
+    @api.onchange("type")
     def onchange_type(self):
         self.size_id = self._default_size_id()
         self.size_value = self._default_size_value()
         self.size_uom = self._default_size_uom()
 
-    @api.onchange('size_id')
+    @api.onchange("size_id")
     def onchange_size_id(self):
         self.size_value = self._default_size_value()
         self.size_uom = self._default_size_uom()
