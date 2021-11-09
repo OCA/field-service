74d7a2e217d0540420bb2befde0d96e665f22f26
Brian McMaster <brian@mcmpest.com>
2021-11-09 10:02:51 -0500

    [IMP] fieldservice_size: black, isort, prettier

--- fieldservice_size/models/fsm_location_size.py
+++ fieldservice_size/models/fsm_location_size.py
@@ -4,20 +4,21 @@ from odoo import fields, models
 
 
 class FSMLocationSize(models.Model):
-    _name = 'fsm.location.size'
-    _description = 'Size for FSM Location'
+    _name = "fsm.location.size"
+    _description = "Size for FSM Location"
 
-    size_id = fields.Many2one('fsm.size', required=True, index=True)
-    type_id = fields.Many2one('fsm.order.type', index=True,
-                              related='size_id.type_id')
-    quantity = fields.Float(string='Quantity', required=True)
-    uom_id = fields.Many2one('uom.uom', index=True,
-                             related='size_id.uom_id')
-    location_id = fields.Many2one('fsm.location', string='Location',
-                                  required=True, index=True)
+    size_id = fields.Many2one("fsm.size", required=True, index=True)
+    type_id = fields.Many2one("fsm.order.type", index=True, related="size_id.type_id")
+    quantity = fields.Float(string="Quantity", required=True)
+    uom_id = fields.Many2one("uom.uom", index=True, related="size_id.uom_id")
+    location_id = fields.Many2one(
+        "fsm.location", string="Location", required=True, index=True
+    )
 
     _sql_constraints = [
-        ('one_size_per_location',
-         'unique(location_id, size_id)',
-         'Only one size of same type allowed per location')
+        (
+            "one_size_per_location",
+            "unique(location_id, size_id)",
+            "Only one size of same type allowed per location",
+        )
     ]
