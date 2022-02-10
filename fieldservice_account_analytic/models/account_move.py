9f20763a33de3705cff957357be10da332fa19fd
Freni Patel <fpatel@opensourceintegrators.com>
2022-02-10 17:21:45 +0530

    [15.0][MIG] fieldservice_account_analytic

--- fieldservice_account_analytic/models/account_move.py
+++ fieldservice_account_analytic/models/account_move.py
@@ -11,14 +11,15 @@ class AccountMoveLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            order = self.env["fsm.order"].browse(vals.get("fsm_order_id"))
-            if order:
-                if order.location_id.analytic_account_id:
-                    vals[
-                        "analytic_account_id"
-                    ] = order.location_id.analytic_account_id.id
-                else:
-                    raise ValidationError(
-                        _("No analytic account " "set on the order's Location.")
-                    )
+            if vals.get("fsm_order_ids") and vals.get("fsm_order_ids")[0][2]:
+                fsm_orders = vals.get("fsm_order_ids")[0][2]
+                for order in self.env["fsm.order"].browse(fsm_orders).exists():
+                    if order.location_id.analytic_account_id:
+                        vals[
+                            "analytic_account_id"
+                        ] = order.location_id.analytic_account_id.id
+                    else:
+                        raise ValidationError(
+                            _("No analytic account " "set on the order's Location.")
+                        )
         return super(AccountMoveLine, self).create(vals_list)
