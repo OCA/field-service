1b4f7f0922ba63273c08ee1ded119fac6c068f42
Yves Goldberg <admin@ygol.com>
2022-04-11 16:58:11 +0300

    fix(fieldservice_project) address PR remark

--- fieldservice_project/__manifest__.py
+++ fieldservice_project/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Field Service - Project",
     "summary": "Create field service orders from a project or project task",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Pavlov Media, Odoo Community Association (OCA)",
     "category": "Project",
