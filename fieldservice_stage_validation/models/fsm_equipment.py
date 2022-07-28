a4b63e77d93801788a27db639954b79a5e714298
Maksym Yankin <yankinmk@gmail.com>
2022-07-28 09:48:42 +0300

    [MIG] fieldservice_stage_validation: Migration to 14.0

--- fieldservice_stage_validation/models/fsm_equipment.py
+++ fieldservice_stage_validation/models/fsm_equipment.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2020 Brian McMaster <brian@mcmpest.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import _, api, models
-from odoo.exceptions import ValidationError
+from odoo import api, models
+
+from .validate_utils import validate_stage_fields
 
 
 class FSMEquipment(models.Model):
@@ -9,17 +10,4 @@ class FSMEquipment(models.Model):
 
     @api.constrains("stage_id")
     def _validate_stage_fields(self):
-        for rec in self:
-            stage = rec.stage_id
-            field_ids = stage.validate_field_ids
-            field_names = [x.name for x in field_ids]
-            values = rec.read(field_names)
-
-            for name in field_names:
-                if not values[0][name]:
-                    raise ValidationError(
-                        _(
-                            'Cannot move to stage "%s" '
-                            'until the "%s" field is set.' % (stage.name, name)
-                        )
-                    )
+        validate_stage_fields(self)
