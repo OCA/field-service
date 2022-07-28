03b8fd7852a43179af12f8ae6a3f7792a0fcbe80
Maksym Yankin <yankinmk@gmail.com>
2022-07-28 09:21:04 +0300

    [IMP] fieldservice_stage_validation: black, isort, prettier

--- fieldservice_stage_validation/__manifest__.py
+++ fieldservice_stage_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "FSM Stage Validation",
     "summary": "Validate input data when reaching a Field Service stage",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Field Service",
     "author": "Brian McMaster, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/field-service",
