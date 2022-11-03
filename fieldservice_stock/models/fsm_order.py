d27459ee7734cc88fbf18fae3d9667b887cc497e
Goncalo Brito <gbrito@erpopen.nl>
2022-11-03 08:37:38 +0100

    [MIG] fieldservice_stock: Migration to 16.0

--- fieldservice_stock/models/fsm_order.py
+++ fieldservice_stock/models/fsm_order.py
@@ -9,9 +9,9 @@ class FSMOrder(models.Model):
 
     @api.model
     def _default_warehouse_id(self):
-        company = self.env.user.company_id.id
+        company = self.env.user.company_id
         warehouse_ids = self.env["stock.warehouse"].search(
-            [("company_id", "=", company)], limit=1
+            [("company_id", "=", company.id)], limit=1
         )
         return warehouse_ids and warehouse_ids.id
 
