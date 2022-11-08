1e952935bf2a1560e3f5104d020cb7c5351e9d5b
ShwetaSerpentCS <s.goltar.serpentcs@gmail.com>
2022-11-08 11:14:24 +0530

    [FIX] fieldservice_skill: test

--- fieldservice_skill/tests/test_fsm_skill.py
+++ fieldservice_skill/tests/test_fsm_skill.py
@@ -2,68 +2,67 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 from odoo.exceptions import ValidationError
-from odoo.tests import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestFSMSkill(SavepointCase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestFSMSkill, cls).setUpClass()
+class TestFSMSkill(TransactionCase):
+    def setUp(self):
+        super(TestFSMSkill, self).setUp()
 
-        cls.skill = cls.env["hr.skill"]
-        cls.skill_level = cls.env["hr.skill.level"]
-        cls.skill_type = cls.env["hr.skill.type"]
-        cls.fsm_person = cls.env["fsm.person"]
-        cls.fsm_person_skill = cls.env["fsm.person.skill"]
-        cls.fsm_order = cls.env["fsm.order"]
-        cls.fsm_location = cls.env["fsm.location"]
-        cls.fsm_template = cls.env["fsm.template"]
-        cls.fsm_category = cls.env["fsm.category"]
+        self.skill = self.env["hr.skill"]
+        self.skill_level = self.env["hr.skill.level"]
+        self.skill_type = self.env["hr.skill.type"]
+        self.fsm_person = self.env["fsm.person"]
+        self.fsm_person_skill = self.env["fsm.person.skill"]
+        self.fsm_order = self.env["fsm.order"]
+        self.fsm_location = self.env["fsm.location"]
+        self.fsm_template = self.env["fsm.template"]
+        self.fsm_category = self.env["fsm.category"]
 
-        cls.skill_type_01 = cls.skill_type.create({"name": "Field Service Skills"})
+        self.skill_type_01 = self.skill_type.create({"name": "Field Service Skills"})
 
-        cls.skill_type_03 = cls.skill_type.create({"name": "Field Service Skills 3"})
+        self.skill_type_03 = self.skill_type.create({"name": "Field Service Skills 3"})
 
         # Create some great skills
-        cls.skill_01 = cls.skill.create(
-            {"name": "Nunchuck Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_01 = self.skill.create(
+            {"name": "Nunchuck Skills", "skill_type_id": self.skill_type_01.id}
         )
-        cls.skill_02 = cls.skill.create(
-            {"name": "Bow Hunting Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_02 = self.skill.create(
+            {"name": "Bow Hunting Skills", "skill_type_id": self.skill_type_01.id}
         )
-        cls.skill_03 = cls.skill.create(
-            {"name": "Computer Hacking Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_03 = self.skill.create(
+            {"name": "Computer Hacking Skills", "skill_type_id": self.skill_type_01.id}
         )
-        cls.skill_04 = cls.skill.create(
-            {"name": "Sweet Bike Owning Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_04 = self.skill.create(
+            {"name": "Sweet Bike Owning Skills", "skill_type_id": self.skill_type_01.id}
         )
-        cls.skill_05 = cls.skill.create(
+        self.skill_05 = self.skill.create(
             {
                 "name": "Hooking Up with Chicks Skills",
-                "skill_type_id": cls.skill_type_01.id,
+                "skill_type_id": self.skill_type_01.id,
             }
         )
-        cls.skill_06 = cls.skill.create(
-            {"name": "Moustache Growing Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_06 = self.skill.create(
+            {"name": "Moustache Growing Skills", "skill_type_id": self.skill_type_01.id}
         )
-        cls.skill_07 = cls.skill.create(
-            {"name": "Growing Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_07 = self.skill.create(
+            {"name": "Growing Skills", "skill_type_id": self.skill_type_01.id}
         )
-        cls.skill_08 = cls.skill.create(
-            {"name": "Computer Growing Skills", "skill_type_id": cls.skill_type_01.id}
+        self.skill_08 = self.skill.create(
+            {"name": "Computer Growing Skills", "skill_type_id": self.skill_type_01.id}
         )
 
-        cls.skill_level_100 = cls.skill_level.create(
+        self.skill_level_100 = self.skill_level.create(
             {
                 "name": "Great",
-                "skill_type_id": cls.skill_type_01.id,
+                "skill_type_id": self.skill_type_01.id,
                 "level_progress": 100,
             }
         )
-        cls.skill_level_101 = cls.skill_level.create(
+        self.skill_level_101 = self.skill_level.create(
             {
                 "name": "Great",
-                "skill_type_id": cls.skill_type_01.id,
+                "skill_type_id": self.skill_type_01.id,
                 "level_progress": 100,
             }
         )
@@ -72,97 +71,99 @@ class TestFSMSkill(SavepointCase):
         # Our first worker, Napoleon, has nunchuck skills and bow hunting
         # skills, which he learned while in Alaska hunting wolverines with his
         # uncle.
-        cls.person_01 = cls.fsm_person.create({"name": "Napoleon"})
-        cls.person_01_skill_01 = cls.fsm_person_skill.create(
+        self.person_01 = self.fsm_person.create({"name": "Napoleon"})
+        self.person_01_skill_01 = self.fsm_person_skill.create(
             {
-                "person_id": cls.person_01.id,
-                "skill_id": cls.skill_01.id,
-                "skill_level_id": cls.skill_level_100.id,
-                "skill_type_id": cls.skill_type_01.id,
+                "person_id": self.person_01.id,
+                "skill_id": self.skill_01.id,
+                "skill_level_id": self.skill_level_100.id,
+                "skill_type_id": self.skill_type_01.id,
             }
         )
-        cls.person_01_skill_02 = cls.fsm_person_skill.create(
+        self.person_01_skill_02 = self.fsm_person_skill.create(
             {
-                "person_id": cls.person_01.id,
-                "skill_id": cls.skill_02.id,
-                "skill_level_id": cls.skill_level_100.id,
-                "skill_type_id": cls.skill_type_01.id,
+                "person_id": self.person_01.id,
+                "skill_id": self.skill_02.id,
+                "skill_level_id": self.skill_level_100.id,
+                "skill_type_id": self.skill_type_01.id,
             }
         )
 
         # Our second worker, Pedro, has a lot of really good skills which he
         # learned from his cousins that have all the sweet hookups
-        cls.person_02 = cls.fsm_person.create({"name": "Pedro"})
-        cls.person_02_skill_04 = cls.fsm_person_skill.create(
+        self.person_02 = self.fsm_person.create({"name": "Pedro"})
+        self.person_02_skill_04 = self.fsm_person_skill.create(
             {
-                "person_id": cls.person_02.id,
-                "skill_id": cls.skill_04.id,
-                "skill_level_id": cls.skill_level_100.id,
-                "skill_type_id": cls.skill_type_01.id,
+                "person_id": self.person_02.id,
+                "skill_id": self.skill_04.id,
+                "skill_level_id": self.skill_level_100.id,
+                "skill_type_id": self.skill_type_01.id,
             }
         )
-        cls.person_02_skill_05 = cls.fsm_person_skill.create(
+        self.person_02_skill_05 = self.fsm_person_skill.create(
             {
-                "person_id": cls.person_02.id,
-                "skill_id": cls.skill_05.id,
-                "skill_level_id": cls.skill_level_100.id,
-                "skill_type_id": cls.skill_type_01.id,
+                "person_id": self.person_02.id,
+                "skill_id": self.skill_05.id,
+                "skill_level_id": self.skill_level_100.id,
+                "skill_type_id": self.skill_type_01.id,
             }
         )
-        cls.person_02_skill_06 = cls.fsm_person_skill.create(
+        self.person_02_skill_06 = self.fsm_person_skill.create(
             {
-                "person_id": cls.person_02.id,
-                "skill_id": cls.skill_06.id,
-                "skill_level_id": cls.skill_level_100.id,
-                "skill_type_id": cls.skill_type_01.id,
+                "person_id": self.person_02.id,
+                "skill_id": self.skill_06.id,
+                "skill_level_id": self.skill_level_100.id,
+                "skill_type_id": self.skill_type_01.id,
             }
         )
 
         # Create a location for an order
-        cls.location_01 = cls.fsm_location.create(
+        self.location_01 = self.fsm_location.create(
             {
                 "name": "Summer's House",
-                "owner_id": cls.env["res.partner"]
+                "owner_id": self.env["res.partner"]
                 .create({"name": "Summer's Parents"})
                 .id,
             }
         )
 
         # Create a category that requires great skills
-        cls.category_01_skills = [cls.skill_04.id, cls.skill_05.id, cls.skill_06.id]
-        cls.category_01 = cls.fsm_category.create(
-            {"name": "Sales", "skill_ids": [(6, 0, cls.category_01_skills)]}
+        self.category_01_skills = [self.skill_04.id, self.skill_05.id, self.skill_06.id]
+        self.category_01 = self.fsm_category.create(
+            {"name": "Sales", "skill_ids": [(6, 0, self.category_01_skills)]}
         )
-        cls.category_02_skills = [cls.skill_05.id, cls.skill_06.id, cls.skill_07.id]
-        cls.category_02 = cls.fsm_category.create(
-            {"name": "Sales1", "skill_ids": [(6, 0, cls.category_02_skills)]}
+        self.category_02_skills = [self.skill_05.id, self.skill_06.id, self.skill_07.id]
+        self.category_02 = self.fsm_category.create(
+            {"name": "Sales1", "skill_ids": [(6, 0, self.category_02_skills)]}
         )
-        cls.skill_type_02 = cls.skill_type.create(
+        self.skill_type_02 = self.skill_type.create(
             {
                 "name": "Field Service Skills 2",
-                "skill_ids": [(6, 0, cls.category_02_skills)],
+                "skill_ids": [(6, 0, self.category_02_skills)],
             }
         )
         # Create a template that requires great skills
-        cls.template_01_skills = [cls.skill_01.id, cls.skill_02.id]
-        cls.template_01 = cls.fsm_template.create(
-            {"name": "Template Name", "skill_ids": [(6, 0, cls.template_01_skills)]}
+        self.template_01_skills = [self.skill_01.id, self.skill_02.id]
+        self.template_01 = self.fsm_template.create(
+            {"name": "Template Name", "skill_ids": [(6, 0, self.template_01_skills)]}
         )
 
         # Create an order that requires no skills
-        cls.order_no_skills = cls.fsm_order.create({"location_id": cls.location_01.id})
+        self.order_no_skills = self.fsm_order.create(
+            {"location_id": self.location_01.id}
+        )
 
         # Create an order with a category
-        cls.order_category_skills = cls.fsm_order.create(
+        self.order_category_skills = self.fsm_order.create(
             {
-                "location_id": cls.location_01.id,
-                "category_ids": [(6, 0, [cls.category_01.id])],
+                "location_id": self.location_01.id,
+                "category_ids": [(6, 0, [self.category_01.id])],
             }
         )
 
         # Create an order with a template
-        cls.order_template_skills = cls.fsm_order.create(
-            {"location_id": cls.location_01.id, "template_id": cls.template_01.id}
+        self.order_template_skills = self.fsm_order.create(
+            {"location_id": self.location_01.id, "template_id": self.template_01.id}
         )
 
     def test_fsm_skills(self):
