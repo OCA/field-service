016057dca61cf1b2ab2855509ed774f54f5f9118
ShwetaSerpentCS <s.goltar.serpentcs@gmail.com>
2022-11-09 18:40:37 +0530

    [IMP] fieldservice_vehicle_stock

--- fieldservice_vehicle_stock/tests/test_product_putaway.py
+++ fieldservice_vehicle_stock/tests/test_product_putaway.py
@@ -49,10 +49,24 @@ class TestStockPutawayRule(TransactionCase):
                 "company_id": self.company1.id,
             }
         )
+
+        brand = self.env["fleet.vehicle.model.brand"].create(
+            {
+                "name": "Jaguar",
+            }
+        )
+
+        model = self.env["fleet.vehicle.model"].create(
+            {
+                "brand_id": brand.id,
+                "name": "J3",
+            }
+        )
         self.vehicle_id = self.env["fsm.vehicle"].create(
             {
                 "name": "Vehicle 1",
                 "inventory_location_id": self.vehicle_location.id,
+                "model_id": model.id,
             }
         )
         self.stock_putaway_rule = self.StockPutawayRule.create(
