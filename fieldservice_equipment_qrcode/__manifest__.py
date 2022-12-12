882d26dafface3d1a86d7447d0a88794cd4210f0
Rafnix Guzmán <rafnixg@gmail.com>
2022-12-12 19:20:06 -0300

    ADD: fieldservice_equipment_qrcode base module

---
+++ fieldservice_equipment_qrcode/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Rafnix Guzman
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Fieldservice Equipment Qrcode',
+    'description': """
+        This module is an add-on for the Field Service application in Odoo.
+        It allows you go to your FSM Equipments print QRcode with URL to fieldserice_equipment_website.""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Rafnix Guzman',
+    'depends': [
+        'fieldservice_equipment_website'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ fieldservice_equipment_qrcode/models/fsm_equiment.py
@@ -0,0 +1,29 @@
+# Copyright 2022 Rafnix Guzman
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+import base64
+import io
+
+
+class FsmEquimentQr(models.Model):
+
+    _inherit = "fsm.equipment"
+
+    qrcode = fields.Binary(
+        attachment=False, store=True, readonly=True,
+        compute='_compute_qrcode',
+    )
+
+    @api.depends('lot_id', 'product_id')
+    def _compute_qrcode(self):
+        for equipment  in self:
+            equipment_qrcode = self._generate_qrcode_from_url(equipment.url)
+            equipment.qrcode = equipment_qrcode
+
+
+    def _generate_qrcode_from_url(self, url):
+        data = io.BytesIO()
+        import qrcode
+        qrcode.make(url, box_size=4).save(data, optimise=True, format='PNG')
+        return base64.b64encode(data.getvalue()).decode()
