f4060a13ad705603123c41514fd59aa25c8073dc
Urvisha-SCS <urvisha.desai@serpentcs.com>
2022-12-15 13:24:08 +0530

    [FIX] codepatch not covered by test

--- fieldservice_geoengine/tests/test_fsm_location.py
+++ fieldservice_geoengine/tests/test_fsm_location.py
@@ -11,6 +11,8 @@ class TestFsmLocation(TransactionCase):
         self.fsm_location = self.env["fsm.location"]
         self.location_partner_1 = self.env.ref("fieldservice.location_partner_1")
         self.location_partner_2 = self.env.ref("fieldservice.location_partner_2")
+        self.test_loc_partner = self.env.ref("fieldservice.test_loc_partner")
+        self.location_partner_3 = self.env.ref("fieldservice.location_partner_3")
 
     def test_fsm_location(self):
         test_location = self.fsm_location.create(
@@ -23,6 +25,7 @@ class TestFsmLocation(TransactionCase):
             }
         )
         self.assertTrue(test_location.shape)
+        fsm_order = self.env["fsm.order"].create({"location_id": test_location.id})
         test_location.write(
             {
                 "date_localization": fields.Datetime.today(),
@@ -33,26 +36,17 @@ class TestFsmLocation(TransactionCase):
         self.assertTrue(test_location.partner_latitude)
         self.assertTrue(test_location.partner_longitude)
         self.assertTrue(test_location.shape)
+        fsm_order.geo_localize()
 
+        self.test_loc_partner.partner_latitude = 1.0
+        self.test_loc_partner.partner_longitude = 2.0
         test_location1 = self.fsm_location.create(
             {
                 "name": "Test Location 3",
                 "phone": "1234",
                 "email": "tl1@email.com",
-                "partner_id": self.location_partner_1.id,
-                "owner_id": self.location_partner_2.id,
-                "partner_latitude": 1.00,
-                "partner_longitude": 2.00,
+                "partner_id": self.test_loc_partner.id,
+                "owner_id": self.location_partner_3.id,
             }
         )
-        self.assertTrue(test_location1.shape)
-        test_location1.write(
-            {
-                "date_localization": fields.Datetime.today(),
-                "partner_latitude": 3.00,
-                "partner_longitude": 4.00,
-            }
-        )
-        self.assertTrue(test_location1.partner_latitude)
-        self.assertTrue(test_location1.partner_longitude)
         self.assertTrue(test_location1.shape)
