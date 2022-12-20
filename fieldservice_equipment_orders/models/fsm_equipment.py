507d1b27a080146f0a697096a1316a9f89539092
Rafnix Guzmán <rafnixg@gmail.com>
2022-12-20 19:38:16 -0300

    FIX: pre-commit issues

--- fieldservice_equipment_orders/models/fsm_equipment.py
+++ fieldservice_equipment_orders/models/fsm_equipment.py
@@ -1,17 +1,17 @@
 # Copyright 2022 Rafnix Guzman
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FsmEquimentOrders(models.Model):
 
     _inherit = "fsm.equipment"
-    
-    fsm_order_ids = fields.One2many('fsm.order', 'equipment_id', 'Orders')
+
+    fsm_order_ids = fields.One2many("fsm.order", "equipment_id", "Orders")
 
     fsm_order_count = fields.Integer(compute="_compute_fsm_order_count")
-    
+
     def _compute_fsm_order_count(self):
         for fsm_equipment in self:
             fsm_equipment.fsm_order_count = len(fsm_equipment.fsm_order_ids)
@@ -19,8 +19,12 @@ class FsmEquimentOrders(models.Model):
     def smart_button_fsm_orders(self):
         self.ensure_one()
 
-        result = self.env['ir.actions.act_window']._for_xml_id('fieldservice.action_fsm_dash_order')
-        result.update({
-            'domain': [('id', 'in', self.fsm_order_ids.ids)],
-        })
+        result = self.env["ir.actions.act_window"]._for_xml_id(
+            "fieldservice.action_fsm_dash_order"
+        )
+        result.update(
+            {
+                "domain": [("id", "in", self.fsm_order_ids.ids)],
+            }
+        )
         return result
