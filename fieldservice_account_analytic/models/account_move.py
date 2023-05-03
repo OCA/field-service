acd0218da26d4a1d97b28867143eb8e8725cd372
Juliette BLANC <juliette.blanc@camptocamp.com>
2023-05-03 10:50:09 +0200

    [MIG] fieldservice_account_analytic: Migration to 16.0

--- fieldservice_account_analytic/models/account_move.py
+++ fieldservice_account_analytic/models/account_move.py
@@ -15,9 +15,9 @@ class AccountMoveLine(models.Model):
                 fsm_orders = vals.get("fsm_order_ids")[0][2]
                 for order in self.env["fsm.order"].browse(fsm_orders).exists():
                     if order.location_id.analytic_account_id:
-                        vals[
-                            "analytic_account_id"
-                        ] = order.location_id.analytic_account_id.id
+                        vals["analytic_distribution"] = {
+                            order.location_id.analytic_account_id.id: 100
+                        }
                     else:
                         raise ValidationError(
                             _("No analytic account " "set on the order's Location.")
