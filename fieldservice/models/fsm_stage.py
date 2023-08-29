00ba838db69f5f3fe14e56a916d24ed469114858
tafaRU <alex.comba@agilebg.com>
2023-08-29 15:25:47 +0200

    [FIX] fieldservice: prevent KeyError: 'stage_type'
    Steps to reproduce it:
    
    * open dashboard Field Service Orders
    * click "Add a Column" in the kanban view
    * type a new stage name

--- fieldservice/models/fsm_stage.py
+++ fieldservice/models/fsm_stage.py
@@ -84,10 +84,9 @@ class FSMStage(models.Model):
     def create(self, vals):
         stages = self.env["fsm.stage"].search([])
         for stage in stages:
-            if (
-                stage.stage_type == vals["stage_type"]
-                and stage.sequence == vals["sequence"]
-            ):
+            if stage.stage_type == vals.get(
+                "stage_type"
+            ) and stage.sequence == vals.get("sequence"):
                 raise ValidationError(
                     _(
                         "Cannot create FSM Stage because "
