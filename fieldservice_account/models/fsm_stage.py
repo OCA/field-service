530e0f844a32a5edeb96051ebde989005f69e04b
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-01-15 10:04:10 -0600

    [ADD] from v14: _get_invoiceable_stage method & fsm_order_ids field to invoice line

--- fieldservice_account/models/fsm_stage.py
+++ fieldservice_account/models/fsm_stage.py
@@ -8,3 +8,11 @@ class FSMStage(models.Model):
     _inherit = "fsm.stage"
 
     is_invoiceable = fields.Boolean(copy=False)
+
+    def _get_invoiceable_stage(self):
+        """
+        override this method to define invoiceable stage
+        by other criteria
+        :return:
+        """
+        return self.search(["is_invoiceable", "=", "True"])
