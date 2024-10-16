074ee4bc1e9b03131d80e3d829517433b9d85107
EdgarRetes <edgar.retes24@gmail.com>
2024-10-16 15:28:36 -0600

    [MIG] fieldservice_isp_flow: Migration to 18.0

--- fieldservice_isp_flow/tests/test_fsm_isp_flow.py
+++ fieldservice_isp_flow/tests/test_fsm_isp_flow.py
@@ -5,7 +5,8 @@ from datetime import timedelta
 
 from odoo import fields
 from odoo.exceptions import ValidationError
-from odoo.tests.common import Form, TransactionCase
+from odoo.tests import Form
+from odoo.tests.common import TransactionCase
 
 
 class FSMIspFlowCase(TransactionCase):
