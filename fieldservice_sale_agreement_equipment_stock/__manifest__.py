5caa1d2a806ed425fdea49505ecd1ca8b8c2c2ef
Iván Todorovich <ivan.todorovich@gmail.com>
2024-12-02 14:45:20 -0300

    [ADD] fieldservice_sale_agreement_equipment_stock

---
+++ fieldservice_sale_agreement_equipment_stock/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Camptocamp SA (https://www.camptocamp.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Field Service - Sale Agreements and Stock Equipment",
+    "summary": "Integrate Field Service with Sale Agreements and Stock Equipment",
+    "version": "17.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/field-service",
+    "license": "AGPL-3",
+    "category": "Field Service",
+    "depends": [
+        "agreement_sale",
+        "fieldservice_sale",
+        "fieldservice_equipment_stock",
+        "sale_stock",
+    ],
+    "auto_install": True,
+}
