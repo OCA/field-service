e1612cad17f60d26cec542d8cc420bf0d01fb19d
Iván Todorovich <ivan.todorovich@gmail.com>
2024-12-03 11:29:40 -0300

    [IMP] fieldservice_size: use computed fields instead of onchanges

--- fieldservice_size/models/fsm_order.py
+++ fieldservice_size/models/fsm_order.py
@@ -6,17 +6,48 @@ from odoo import api, fields, models
 class FSMOrder(models.Model):
     _inherit = "fsm.order"
 
-    def _default_size_id(self):
-        size = False
-        if self.type:
-            size = self.env["fsm.size"].search(
-                [("type_id", "=", self.type.id), ("is_order_size", "=", True)], limit=1
-            )
-        return size
+    size_id = fields.Many2one(
+        "fsm.size",
+        compute="_compute_size_id",
+        precompute=True,
+        readonly=False,
+        store=True,
+    )
+    size_value = fields.Float(
+        string="Order Size",
+        compute="_compute_size_value",
+        precompute=True,
+        readonly=False,
+        store=True,
+    )
+    size_uom_category = fields.Many2one(
+        string="Unit of Measure Category",
+        related="size_id.uom_id.category_id",
+    )
+    size_uom = fields.Many2one(
+        "uom.uom",
+        string="Unit of Measure",
+        domain="[('category_id', '=?', size_uom_category)]",
+        compute="_compute_size_uom",
+        precompute=True,
+        readonly=False,
+        store=True,
+    )
 
-    def _default_size_value(self):
-        size_value = 0
-        if self.size_id:
+    @api.depends("type")
+    def _compute_size_id(self):
+        for rec in self:
+            if rec.type:
+                rec.size_id = self.env["fsm.size"].search(
+                    [("type_id", "=", rec.type.id), ("is_order_size", "=", True)],
+                    limit=1,
+                )
+
+    @api.depends("size_id", "location_id")
+    def _compute_size_value(self):
+        for rec in self:
+            if not rec.size_id or not rec.location_id:
+                continue
             size = self.env["fsm.location.size"].search(
                 [
                     ("location_id", "=", self.location_id.id),
@@ -24,34 +55,9 @@ class FSMOrder(models.Model):
                 ],
                 limit=1,
             )
-            if size:
-                size_value = size.quantity
-        return size_value
+            rec.size_value = size.quantity
 
-    def _default_size_uom(self):
-        return self.size_id.uom_id if self.size_id else False
-
-    size_id = fields.Many2one("fsm.size", default=_default_size_id)
-    size_value = fields.Float(string="Order Size", default=_default_size_value)
-    size_uom = fields.Many2one(
-        "uom.uom", string="Unit of Measure", default=_default_size_uom
-    )
-
-    @api.onchange("location_id")
-    def _onchange_location_id_customer(self):
-        res = super()._onchange_location_id_customer()
-        self.size_id = self._default_size_id()
-        self.size_value = self._default_size_value()
-        self.size_uom = self._default_size_uom()
-        return res
-
-    @api.onchange("type")
-    def onchange_type(self):
-        self.size_id = self._default_size_id()
-        self.size_value = self._default_size_value()
-        self.size_uom = self._default_size_uom()
-
-    @api.onchange("size_id")
-    def onchange_size_id(self):
-        self.size_value = self._default_size_value()
-        self.size_uom = self._default_size_uom()
+    @api.depends("size_id")
+    def _compute_size_uom(self):
+        for rec in self:
+            rec.size_uom = rec.size_id.uom_id
