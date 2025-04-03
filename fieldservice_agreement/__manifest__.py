f699f94ad0f37e498b8652459902c46ec85b5ade
TB-Ph35 <thomas.bonnerue@akretion.com>
2025-04-03 12:45:35 +0200

    [MIG] fieldservice_agreement: Migration to 18.0

--- fieldservice_agreement/__manifest__.py
+++ fieldservice_agreement/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://github.com/OCA/field-service",
     "category": "Field Service",
     "license": "AGPL-3",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": ["fieldservice", "agreement"],
     "data": [
         "views/fsm_order_view.xml",
