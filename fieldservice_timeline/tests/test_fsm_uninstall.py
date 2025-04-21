25fde4ac258bb9f17cc5aff40fd7185cf0059c21
Bhavesh Heliconia <oca@heliconia.in>
2025-04-21 11:24:17 +0530

    [MIG] fieldservice_timeline: Migration to 17.0

--- fieldservice_timeline/tests/test_fsm_uninstall.py
+++ fieldservice_timeline/tests/test_fsm_uninstall.py
@@ -1,11 +1,10 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
-
+from odoo.addons.base.tests.common import BaseCommon
 from odoo.addons.fieldservice_timeline import hooks
 
 
-class FSMUninstall(TransactionCase):
+class FSMUninstall(BaseCommon):
     def test_fsm_uninstall(self):
         hooks.uninstall_hook(self.env.cr, False)
