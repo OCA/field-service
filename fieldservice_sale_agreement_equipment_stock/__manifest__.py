ada91a0c4d09899f66224ba0690944997caae11b
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2025-08-18 11:48:28 +0200

    [MIG] fieldservice_sale_agreement_equipment_stock: Migration to 18.0

--- fieldservice_sale_agreement_equipment_stock/__manifest__.py
+++ fieldservice_sale_agreement_equipment_stock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Field Service - Sale Agreements and Stock Equipment",
     "summary": "Integrate Field Service with Sale Agreements and Stock Equipment",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/field-service",
@@ -12,6 +12,7 @@
     "category": "Field Service",
     "depends": [
         "agreement_sale",
+        "fieldservice_agreement",
         "fieldservice_sale",
         "fieldservice_equipment_stock",
         "sale_stock",
--- fieldservice_sale_agreement_equipment_stock/tests/test_fsm_sale_agreement_equipment_stock.py
+++ fieldservice_sale_agreement_equipment_stock/tests/test_fsm_sale_agreement_equipment_stock.py
@@ -11,13 +11,19 @@ from odoo.addons.sale.tests.common import TestSaleCommon
 @tagged("post_install", "-at_install")
 class TestFieldServiceSaleAgreementEquipmentStock(TestSaleCommon):
     @classmethod
+    def get_default_groups(cls):
+        groups = super().get_default_groups()
+        return groups | cls.env.ref("fieldservice.group_fsm_manager")
+
+    @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.product = cls.env["product.product"].create(
             {
                 "name": "FSM Product",
-                "type": "product",
+                "type": "consu",
+                "is_storable": True,
                 "tracking": "serial",
                 "create_fsm_equipment": True,
             }
