ffdc7465a4f6aa85c3173be2ee82e523ffea25ea
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2025-08-18 10:33:01 +0200

    [MIG] fieldservice_sale_agreement: Migration to 18.0

--- fieldservice_sale_agreement/__manifest__.py
+++ fieldservice_sale_agreement/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Field Service - Sale Agreements",
     "summary": "Integrate Field Service with Sale Agreements",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/field-service",
