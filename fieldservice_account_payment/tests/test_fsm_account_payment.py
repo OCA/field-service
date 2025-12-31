13526a10129c3d8b09ff68d159cc4d1b38a9344a
r.perez <r.perez@binhex.cloud>
2025-12-31 10:06:44 -0500

    [MIG] fieldservice_account_payment: Migration to 17.0

--- fieldservice_account_payment/tests/test_fsm_account_payment.py
+++ fieldservice_account_payment/tests/test_fsm_account_payment.py
@@ -1,43 +1,102 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Serpent consulting Services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import Command, fields
+from odoo import Command, _, fields
+from odoo.exceptions import ValidationError
 from odoo.tests import tagged
 
-from odoo.addons.fieldservice_account.tests.test_fsm_account import FSMAccountCase
+from odoo.addons.base.tests.common import BaseCommon
 
 
 @tagged("-at_install", "post_install")
-class FSMAccountPaymentCase(FSMAccountCase):
+class FSMAccountPaymentCase(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
-        cls.register_payments_model = cls.env["account.payment.register"].with_context(
-            active_model="account.move"
+        cls.company = cls.env.company
+        chart = cls.env["account.chart.template"]._guess_chart_template(
+            cls.company.country_id
         )
+        if not chart:
+            raise ValidationError(_("No Chart of Account Template has been defined !"))
+
+        cls.env["account.chart.template"].try_loading(
+            "generic_coa", company=cls.company, install_demo=False
+        )
+        cls.register_payments_model = cls.env["account.payment.register"]
         cls.payment_model = cls.env["account.payment"]
 
-        cls.company = cls.env.user.company_id
-
-        cls.bank_journal = (
-            cls.env["account.journal"]
-            .search(
-                [("company_id", "=", cls.company.id), ("type", "=", "bank")], limit=1
-            )
-            .ensure_one()
+        cls.bank_journal = cls.env["account.journal"].search(
+            [("company_id", "=", cls.company.id), ("type", "=", "bank")], limit=1
         )
+
+        # create a Res Partner to be converted to FSM Location/Person
+        cls.test_loc_partner = cls.env["res.partner"].create(
+            {"name": "Test Loc Partner", "phone": "ABC", "email": "tlp@email.com"}
+        )
+
         cls.inbound_payment_method_line = (
             cls.bank_journal.inbound_payment_method_line_ids[0]
         )
 
-        cls.test_invoice.invoice_line_ids.update({"tax_ids": [Command.clear()]})
+        cls.test_location = (
+            cls.env["fsm.location"]
+            .with_context(default_customer_id=cls.partner)
+            .create(
+                {
+                    "name": "Test Location",
+                    "phone": "123",
+                    "email": "tp@email.com",
+                    "partner_id": cls.test_loc_partner.id,
+                    "owner_id": cls.test_loc_partner.id,
+                }
+            )
+        )
+
+        # Create a FSM order
+        cls.test_order = cls.env["fsm.order"].create(
+            {
+                "location_id": cls.test_location.id,
+                "date_start": fields.Date.today(),
+                "date_end": fields.Datetime.add(fields.Date.today(), hours=2),
+                "request_early": fields.Date.today(),
+            }
+        )
+
+        cls.test_order2 = cls.env["fsm.order"].create(
+            {
+                "location_id": cls.test_location.id,
+                "date_start": fields.Date.today(),
+                "date_end": fields.Datetime.add(fields.Date.today(), hours=2),
+                "request_early": fields.Date.today(),
+            }
+        )
+
+        # Create an invoice
+        cls.test_invoice = cls.env["account.move"].create(
+            {
+                "partner_id": cls.partner.id,
+                "move_type": "out_invoice",
+                "invoice_date": fields.Date.today(),
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "name": "Test",
+                            "quantity": 1.00,
+                            "price_unit": 100.00,
+                            "tax_ids": [],
+                        },
+                    )
+                ],
+            }
+        )
+
         cls.test_invoice.action_post()
+        cls.test_order.invoice_lines = [Command.set(cls.test_invoice.line_ids.ids)]
 
     def test_fsm_account_payment(self):
         ctx = {
             "active_model": "account.move",
-            "active_id": self.test_invoice.id,
             "active_ids": self.test_invoice.ids,
         }
         register_payments = self.register_payments_model.with_context(**ctx).create(
@@ -47,14 +106,17 @@ class FSMAccountPaymentCase(FSMAccountCase):
                 "payment_method_line_id": self.inbound_payment_method_line.id,
             }
         )
+        payment = register_payments._create_payments()
         order = self.test_order
-        register_payment_action = register_payments.action_create_payments()
-        payment = self.payment_model.browse(register_payment_action.get("res_id"))
-        test_order = self.test_order.id
-        payment.fsm_order_ids = [Command.set(self.test_order.ids)]
+        payment._compute_fsm_order_ids()
+        self.assertRecordValues(
+            payment,
+            [
+                {"fsm_order_ids": [order.id]},
+            ],
+        )
         payment_copy = payment.copy()
         self.assertNotEqual(payment.fsm_order_ids, payment_copy.fsm_order_ids)
-        self.test_invoice.fsm_order_ids = [Command.set(self.test_order.ids)]
         order.payment_ids = [Command.set(payment.ids)]
         payment.action_view_fsm_orders()
         order.action_view_payments()
@@ -64,9 +126,9 @@ class FSMAccountPaymentCase(FSMAccountCase):
         self.assertEqual(self.test_invoice.fsm_order_ids, payment.fsm_order_ids)
         res = self.env["fsm.order"].search([("payment_ids", "in", payment.id)])
         self.assertEqual(len(res), 1)
-        payment.fsm_order_ids = [Command.set([test_order, self.test_order2.id])]
+        self.test_invoice.fsm_order_ids = [Command.set([order.id, self.test_order2.id])]
+        payment._compute_fsm_order_ids()
         payment.action_view_fsm_orders()
-        register_payment_action2 = register_payments.action_create_payments()
-        payment2 = self.payment_model.browse(register_payment_action2.get("res_id"))
+        payment2 = register_payments._create_payments()
         order.payment_ids = [Command.set([payment.id, payment2.id])]
         order.action_view_payments()
