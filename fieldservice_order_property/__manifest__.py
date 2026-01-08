bf9ad54cf5e142aaa8a775b435c9096d1d2af90c
r.perez <r.perez@binhex.cloud>
2026-01-07 20:32:15 -0500

    [ADD] fieldservice_order_property

---
+++ fieldservice_order_property/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Field Service Order_Property",
+    "summary": "Manage custom properties for Field Service Orders",
+    "author": "Binhex, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/field-service",
+    "category": "Field Service",
+    "version": "17.0.1.0.0",
+    "depends": ["fieldservice"],
+    "data": [
+        "views/fsm_order_views.xml",
+    ],
+    "license": "AGPL-3",
+}
