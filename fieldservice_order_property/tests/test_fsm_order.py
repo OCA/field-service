7d962bc5a89d1aaf8c2a138f37d73741ee0ba094
r.perez <r.perez@binhex.cloud>
2026-05-19 12:49:59 -0400

    test: add failing test for property default values on fsm.order
    Properties defined on fsm.team with a default value are not applied
    to newly created fsm.order records. This test documents the expected
    behaviour before the fix is implemented.

--- fieldservice_order_property/tests/test_fsm_order.py
+++ fieldservice_order_property/tests/test_fsm_order.py
@@ -21,6 +21,7 @@ class TestFSMOrder(BaseCommon):
         cls.team_a.fsm_order_properties_definition = properties_def
         cls.team_b = cls.env["fsm.team"].create({"name": "Team B"})
         cls.test_location = cls.env.ref("fieldservice.test_location")
+        cls.test_location.team_id = cls.team_a
         cls.fsm_order_team_a = cls.env["fsm.order"].create(
             {
                 "name": "Test Order Team A",
@@ -38,3 +39,22 @@ class TestFSMOrder(BaseCommon):
         properties = self.fsm_order_team_a.fsm_order_properties
         self.assertEqual(len(properties), 2)
         self.assertFalse(self.team_b.fsm_order_properties_definition)
+
+    def test_fsm_order_properties_default_value(self):
+        order = self.env["fsm.order"].create(
+            {
+                "name": "Test Default Props",
+                "location_id": self.test_location.id,
+            }
+        )
+        props = order.fsm_order_properties or {}
+        self.assertEqual(
+            props.get("integer"),
+            1,
+            "integer property must carry default value 1 from team definition",
+        )
+        self.assertEqual(
+            props.get("date"),
+            "2026-01-01",
+            "date property must carry default value '2026-01-01' from team definition",
+        )
